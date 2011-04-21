0336a6c8df60a2c3bd3c51f64310ce8c24a8e668
crynobone <crynobone@gmail.com>
2011-04-22 01:50:11 +0800

    Fixed #56 PHP5 method declaration

--- classes/cli.php
+++ classes/cli.php
@@ -347,7 +347,7 @@ class Cli {
 	 * @param	integer	Number of lines to output
 	 * @return	void
 	 */
-	function new_line($num = 1)
+	public static function new_line($num = 1)
 	{
         // Do it once or more, write with empty string gives us a new line
         for($i = 0; $i < $num; $i++)
@@ -361,7 +361,7 @@ class Cli {
 	 *
 	 * @return	void
 	 */
-    function clear_screen()
+    public static function clear_screen()
     {
 		static::is_windows()
 
