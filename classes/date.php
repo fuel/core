b7d1760cfe9c3e85d15a2cb41774ee81b6e58c0d
Dan Horrigan <dan@dhorrigan.com>
2011-04-23 16:07:27 -0400

    Added the ability to send the time as a string to Date::time_ago.  Thanks Zack Kitzmiller.

--- classes/date.php
+++ classes/date.php
@@ -216,22 +216,24 @@ class Date {
 	 */
 	public static function time_ago($timestamp, $from_timestamp = null)
 	{
-        if ($timestamp === null)
+		if ($timestamp === null)
 		{
 			return;
 		}
-		
+
+		! is_int($timestamp) and $timestamp = static::create_from_string($timestamp);
+
 		$from_timestamp == null and $from_timestamp = time();
 		
 		\Lang::load('date', true);
 		
 		$difference = $from_timestamp - $timestamp;
 		$periods	= array('second', 'minute', 'hour', 'day', 'week', 'month', 'years', 'decade');
- 		$lengths	= array(60, 60, 24, 7, 4.35, 12, 10);
+		$lengths	= array(60, 60, 24, 7, 4.35, 12, 10);
 
 		for ($j = 0; $difference >= $lengths[$j]; $j++)
 		{
-        	$difference /= $lengths[$j];
+			$difference /= $lengths[$j];
 		}
 
         $difference = round($difference);
