2a2767d52e43ccea1399a027b4a5116c64c06ec6
Dan Horrigan <dan@dhorrigan.com>
2011-04-23 14:38:31 -0400

    Adding empty unit tests for all core classes.

---
+++ tests/lang.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package    Fuel
+ * @version    1.0
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2011 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+namespace Fuel\Core;
+
+/**
+ * Lang class tests
+ * 
+ * @group Core
+ * @group Lang
+ */
+class Test_Lang extends TestCase {
+ 	public function test_foo() {}
+}
