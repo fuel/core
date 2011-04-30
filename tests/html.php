998fa23ae653875295d15834c51b369f13693a46
Frank Bardon Jr <frank@nerdsrescue.me>
2011-04-30 10:22:55 -0400

    More complete testing for Html::methods

--- tests/html.php
+++ tests/html.php
@@ -136,6 +136,174 @@ class Tests_Html extends TestCase {
 		$this->assertEquals($expected, $output);
 	}
 
+	/**
+	 * Tests Html::anchor()
+	 * 
+	 * @test
+	 */
+	public function test_anchor()
+	{
+		// External uri
+		$output = Html::anchor('http://google.com', 'Go to Google');
+		$expected = '<a href="http://google.com">Go to Google</a>';
+		$this->assertEquals($expected, $output);
+		
+		$output = Html::anchor('javascript:do();', 'Do()');
+		$expected = '<a href="javascript:do();">Do()</a>';
+		$this->assertEquals($expected, $output);
+		
+		$output = Html::anchor('http://google.com', 'Go to Google', array('rel' => 'example', 'class' => 'sample', 'style' => 'color:red;'));
+		$expected = '<a rel="example" class="sample" style="color:red;" href="http://google.com">Go to Google</a>';
+		$this->assertEquals($expected, $output);
+		
+		// Internal uri
+		$output = Html::anchor('controller/method', 'Method');
+		$expected = '<a href="index.php/controller/method">Method</a>';
+		$this->assertEquals($expected, $output);
+	}
+	
+	/**
+	 * Tests Html::img()
+	 * 
+	 * This test does not account for the image file existing in
+	 * the filesystem. There are no images bundled with the framework
+	 * by default, so no reliable test can be run on an actual image.
+	 * 
+	 * @test
+	 */
+	public function test_img()
+	{
+		// Internal uri
+		$output = Html::img('image.png');
+		$expected = '<img src="index.php/image.png" alt="image" />';
+		$this->assertEquals($expected, $output);
+		
+		$output = Html::img('image.png', array('alt' => 'Image'));
+		$expected = '<img alt="Image" src="index.php/image.png" />';
+		$this->assertEquals($expected, $output);
+		
+		// External uri
+		$output = Html::img('http://google.com/image.png');
+		$expected = '<img src="http://google.com/image.png" />';
+	}
+	
+	/**
+	 * Tests Html::prep_url()
+	 * 
+	 * @test
+	 */
+	public function test_prep_url()
+	{
+		$output = Html::prep_url('google.com');
+		$expected = 'http://google.com';
+		$this->assertEquals($expected, $output);
+		
+		$output = Html::prep_url('google.com', 'https');
+		$expected = 'https://google.com';
+		$this->assertEquals($expected, $output);
+	}
+	
+	/**
+	 * Tests Html::mail_to()
+	 * 
+	 * @test
+	 */
+	public function test_mail_to()
+	{
+		$output = Html::mail_to('test@test.com');
+		$expected = '<a href="mailto:test@test.com">test@test.com</a>';
+		$this->assertEquals($expected, $output);
+		
+		$output = Html::mail_to('test@test.com', 'Email');
+		$expected = '<a href="mailto:test@test.com">Email</a>';
+		$this->assertEquals($expected, $output);
+		
+		$output = Html::mail_to('test@test.com', NULL, 'Test');
+		$expected = '<a href="mailto:test@test.com?subject=Test">test@test.com</a>';
+		$this->assertEquals($expected, $output);
+		
+		$output = Html::mail_to('test@test.com', 'Test', 'Test');
+		$expected = '<a href="mailto:test@test.com?subject=Test">Test</a>';
+		$this->assertEquals($expected, $output);
+	}
+	
+	/**
+	 * Tests Html::doctype()
+	 * 
+	 * @test
+	 */
+	public function test_doctype()
+	{
+		$output = Html::doctype();
+		$expected = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
+		$this->assertEquals($expected, $output);
+		
+		$output = Html::doctype('html5');
+		$expected = '<!DOCTYPE html>';
+		$this->assertEquals($expected, $output);
+		
+		// Ensure static::$html5 is set
+		$doctype = Html::doctype('html5');
+		$this->assertTrue(Html::$html5);
+		
+		// Ensure === false if doctype is invalid
+		$this->assertFalse(Html::doctype('shouldfail'));
+	}
+	
+	/**
+	 * Tests Html::header()
+	 * 
+	 * @test
+	 */
+	public function test_header()
+	{
+		// Doctype != html5
+		Html::$html5 = FALSE;
+		
+		$output = Html::header();
+		$expected = '<div id="header"></div>';
+		$this->assertEquals($expected, $output);
+		
+		$output = Html::header('Content');
+		$expected = '<div id="header">Content</div>';
+		$this->assertEquals($expected, $output);
+		
+		// Doctype = html5
+		Html::$html5 = TRUE;
+		$output = Html::header();
+		$expected = '<header></header>';
+		$this->assertEquals($expected, $output);
+		
+		$output = Html::header('Content');
+		$expected = '<header>Content</header>';
+		$this->assertEquals($expected, $output);
+	}
+	
+	/**
+	 * Tests Html::ul() & Html::ol()
+	 * 
+	 * @test
+	 */
+	public function test_lists()
+	{
+		$list = array('one', 'two');
+		
+		$output = Html::ul($list);
+		$expected = '<ul>
+	<li>one</li>
+	<li>two</li>
+</ul>
+';
+		$this->assertEquals($expected, $output);
+		
+		$output = Html::ol($list);
+		$expected = '<ol>
+	<li>one</li>
+	<li>two</li>
+</ol>
+';
+		$this->assertEquals($expected, $output);
+	}
 }
 
 /* End of file html.php */
