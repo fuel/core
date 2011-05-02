35e1b875e58e7b246fa8b9e0146ebe5f4351cbe5
Kris Keyes <dudeami0@gmail.com>
2011-05-02 18:14:33 -0400

    Reverted classes/asset.php

--- classes/asset.php
+++ classes/asset.php
@@ -153,7 +153,7 @@ class Asset {
 			{
 				if ( ! ($file = static::find_file($filename, static::$_folders[$type])))
 				{
-					throw new \Fuel_Exception('Could not find asset: '.$filename);
+					throw new \Fuel_Exception('Could not find asset: '.$file);
 				}
 
 				$file = static::$_asset_url.$file;
