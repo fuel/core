c106ff81e9a07714bb03c1d3fd3557d9ce00ac3e
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2011-05-02 14:50:04 +0200

    Made it possible to get the Validation instance on which a rule is being run, only available while validation is running.

--- classes/validation.php
+++ classes/validation.php
@@ -28,6 +28,11 @@ namespace Fuel\Core;
  */
 class Validation {
 
+	/**
+	 * @var  Validation  keeps a reference to an instance of Validation while it is being run
+	 */
+	protected static $active;
+
 	public static function factory($fieldset = 'default')
 	{
 		if ( ! $fieldset instanceof Fieldset)
@@ -42,6 +47,22 @@ class Validation {
 	{
 		$fieldset = \Fieldset::instance($name);
 		return $fieldset === false ? false : $fieldset->validation();
+	}
+
+	/**
+	 * Fetch the currently active validation instance
+	 */
+	public static function active()
+	{
+		return static::$active;
+	}
+
+	/**
+	 * Set or unset the currently active validation instance
+	 */
+	protected static function set_active($instance = null)
+	{
+		static::$active = $instance;
 	}
 
 	/**
@@ -210,6 +231,8 @@ class Validation {
 			return false;
 		}
 
+		static::set_active($this);
+
 		$this->validated = array();
 		$this->errors = array();
 		$this->input = $input ?: array();
@@ -236,6 +259,8 @@ class Validation {
 				$this->errors[$field->name] = $v;
 			}
 		}
+
+		static::set_active();
 
 		return empty($this->errors);
 	}
