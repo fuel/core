27adb82778bf04a34588e55a0cd2b68d37a0a76f
Tom Schlick <tom@tomschlick.com>
2011-05-04 00:20:48 -0400

    added a random string method & associated unit tests

--- classes/str.php
+++ classes/str.php
@@ -164,6 +164,74 @@ class Str {
 			? mb_convert_case($str, MB_CASE_TITLE, $encoding)
 			: ucwords(strtolower($str));
 	}
+	
+	/**
+	  * Creates a random string of characters
+	  *
+	  * @param	string	the type of string
+	  * @param	int		the number of characters
+	  * @return string	the random string
+	  */
+	public static function random($type = 'alnum', $length = 16)
+	{
+		switch($type)
+		{
+			case 'basic': 
+				return mt_rand();
+				break;
+
+			default:
+			case 'alnum':
+			case 'numeric':
+			case 'nozero':
+			case 'alpha':
+			case 'distinct':
+			case 'hexdec':
+				switch ($type)
+				{
+					case 'alpha':
+						$pool = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+						break;
+
+					default:
+					case 'alnum':
+						$pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+						break;
+
+					case 'numeric':
+						$pool = '0123456789';
+						break;
+
+					case 'nozero':
+						$pool = '123456789';
+						break;
+
+					case 'distinct':
+						$pool = '2345679ACDEFHJKLMNPRSTUVWXYZ';
+						break;
+
+					case 'hexdec':
+						$pool = '0123456789abcdef';
+						break;
+				}
+
+				$str = '';
+				for ($i=0; $i < $length; $i++)
+				{
+					$str .= substr($pool, mt_rand(0, strlen($pool) -1), 1);
+				}
+				return $str;
+				break;
+
+			case 'unique':
+				return md5(uniqid(mt_rand()));
+				break;
+
+			case 'sha1' :
+				return sha1(uniqid(mt_rand(), true));
+				break;
+		}
+	}
 }
 
 /* End of file str.php */
