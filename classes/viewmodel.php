0d5ce6e7e39d190daffb3ffe891eef0036d8a9b1
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2011-05-25 15:12:13 +0200

    Invalid ViewModel now throws SPL OutOfBoundsException.

--- classes/viewmodel.php
+++ classes/viewmodel.php
@@ -41,7 +41,7 @@ abstract class ViewModel {
 		{
 			if ( ! class_exists($class = $viewmodel))
 			{
-				throw new \Fuel_Exception('ViewModel could not be found.');
+				throw new \OutOfBoundsException('ViewModel could not be found.');
 			}
 		}
 
