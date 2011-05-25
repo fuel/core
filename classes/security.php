e3bf025d5a3bab68ab45355f9be1bdd2da32b6f5
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2011-05-25 15:06:59 +0200

    Error thrown on non-whitelisted object than can't be cast to string now a SPL RuntimeException.

--- classes/security.php
+++ classes/security.php
@@ -175,7 +175,7 @@ class Security {
 			// Throw exception when it wasn't whitelisted and can't be converted to String
 			if ( ! method_exists($value, '__toString'))
 			{
-				throw new \Fuel_Exception('Object class "'.get_class($value).'" could not be converted to string or '.
+				throw new \RuntimeException('Object class "'.get_class($value).'" could not be converted to string or '.
 					'sanitized as ArrayAcces. Whitelist it in security.whitelisted_classes in app/config/config.php '.
 					'to allow it to be passed unchecked.');
 			}
