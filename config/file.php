8c33812fa1850e55d24635856795c370f06ba004
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2011-06-06 02:13:05 +0200

    Updated base File class config file to use handler instead of driver.

--- config/file.php
+++ config/file.php
@@ -14,41 +14,41 @@ return array(
 
 	// The default File_Area config
 	'base_config' => array(
-		
+
 		/**
-		 *	Path to basedir restriction, null for no restriction
+		 * Path to basedir restriction, null for no restriction
 		 */
-		'basedir'		=> null,
-		
+		'basedir'  => null,
+
 		/**
-		 *	Array of allowed extensions, null for all
+		 * Array of allowed extensions, null for all
 		 */
-		'extensions'	=> null,
-		
+		'extensions'  => null,
+
 		/**
-		 *	Base url for files, null for not available
+		 * Base url for files, null for not available
 		 */
-		'url'			=> null,
-		
+		'url'  => null,
+
 		/**
-		 *	Whether or not to use file locks when doing file operations
+		 * Whether or not to use file locks when doing file operations
 		 */
-		'use_locks'		=> null,
-		
+		'use_locks'  => null,
+
 		/**
-		 *	array containing file driver per file extension
+		 * array containing file driver per file extension
 		 */
-		'file_drivers'		=> array(),
+		'file_handlers'  => array(),
 	),
-	
-	
+
+	// Pre configure some areas
 	'areas' => array(
 		/* 'area_name' => array(
-			'basedir'		=> null,
-			'extensions'	=> null,
-			'url'			=> null,
-			'use_locks'		=> null,
-			'file_drivers'	=> array(),
+			'basedir'        => null,
+			'extensions'     => null,
+			'url'            => null,
+			'use_locks'      => null,
+			'file_handlers'  => array(),
 		), */
 	),
 );
