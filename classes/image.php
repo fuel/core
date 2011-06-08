9b87142edbbb0e459032262901f0f65829bd2c32
johnnyfreeman <prsjohnny@gmail.com>
2011-06-08 06:14:35 -0700

    Switched to using array_merge so that the each default config could be overwritten individually. Also, used the Config class in the global namespace so that it will call the replacement class if it exists.

--- classes/image.php
+++ classes/image.php
@@ -48,11 +48,10 @@ class Image {
 	public static function factory($config = array(), $filename = null)
 	{
 		!is_array($config) and $config = array();
-		if (empty($config))
-		{
-			Config::load('image', 'image');
-			$config = Config::get('image');
-		}
+
+		\Config::load('image', 'image');
+		$config = array_merge(\Config::get('image'), $config);
+
 		$protocol = ucfirst( ! empty($config['driver']) ? $config['driver'] : 'gd');
 		$class = 'Image_'.$protocol;
 		if ($protocol == 'Driver' || ! class_exists($class))
