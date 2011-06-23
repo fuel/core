03a8c055e6fe147b014d467d1756f287e6246e5b
Chase Hutchins <syntaqx@gmail.com>
2011-06-23 11:43:39 -0600

    Updated the num helper with the Fuel\Core namespace.

--- classes/num.php
+++ classes/num.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package    Fuel
+ * @version    1.0
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2011 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+namespace Fuel\Core;
 
 /**
  * Number helper class. Provides additional formatting methods for working with
