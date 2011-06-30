a5aae89a13070dc8730ea10ef0d7d115e0a642d0
Calvin Froedge <calvinfroedge@gmail.com>
2011-06-30 00:27:11 -0500

    updated comment regarding database name for database sessions config to improve clarity

--- config/session.php
+++ config/session.php
@@ -81,7 +81,7 @@ return array(
 	// specific configuration settings for database based sessions
 	'db'			=> array(
 		'cookie_name'		=> 'fueldid',				// name of the session cookie for database based sessions
-		'database'			=> null,					// name of the database name (as configured in config/db.php)
+		'database'			=> null,					// name of database.  Leave as null to get configuration from config/db.php
 		'table'				=> 'sessions',				// name of the sessions table
 		'gc_probability'	=> 5						// probability % (between 0 and 100) for garbage collection
 						),
