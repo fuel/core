04e778e1570e1752a9546d186d05f4b5fb9bcfce
WanWizard <wanwizard@wanwizard.eu>
2011-07-18 10:44:11 +0200

    added a notice to the default config files to copy them to app/config before modifying them.

--- config/migrations.php
+++ config/migrations.php
@@ -9,6 +9,16 @@
  * @copyright	2010 - 2011 Fuel Development Team
  */
 
+/**
+ * NOTICE:
+ *
+ * If you need to make modifications to the default configuration, copy
+ * this file to your app/config folder, and make them in there.
+ *
+ * This will allow you to upgrade fuel without losing your custom config.
+ */
+
+
 return array(
 
 	/*
