211b9b95d02735659a1b54a31a30fcac0ecfe60e
Chase Hutchins <syntaqx@gmail.com>
2011-07-25 12:51:09 -0600

    Added base concept for language line and sprintf-like statements

--- classes/fuel/exception.php
+++ classes/fuel/exception.php
@@ -12,4 +12,39 @@
 
 namespace Fuel\Core;
 
-class Fuel_Exception extends \Exception {}
+/**
+ * Default exception, mainly used for general errors. All Fuel specific
+ * exceptions extend this exception
+ *
+ * @author     Chase "Syntaqx" Hutchins <syntaqx@gmail.com>
+ * @version    1.0
+ * @package    Fuel
+ */
+class Fuel_Exception extends \Exception {
+
+	public function __construct()
+	{
+		if(($count = \func_num_args()) === 0)
+		{
+			parent::__construct('Unknown error');
+		}
+		else
+		{
+			$args = \func_get_args();
+
+			if($count == 2 and is_array($args[1]))
+			{
+				$line = \Lang::line($args[0], $args[1]);
+
+				if($line != $args[1])
+				{
+					$args = array($line);
+				}
+			}
+
+			parent::__construct(\call_user_func_array('\sprintf', $args));
+		}
+	}
+}
+
+/* End of file exception.php */
