d8bf57cdbdb87406d58d4add42e9d66efd0aab87
Tom Arnfeld <tarnfeld@me.com>
2011-08-01 20:22:40 +0100

    removed static from methods that are all using

--- tests/num.php
+++ tests/num.php
@@ -26,7 +26,7 @@ class Tests_Num extends TestCase {
 	/**
 	 * @see     Num::bytes
 	 */
-	public static function test_bytes($size = 0)
+	public function test_bytes($size = 0)
 	{
 		$output = Num::bytes('200K');
 		$expected = '204800';
@@ -37,7 +37,7 @@ class Tests_Num extends TestCase {
 	/**
 	 * @see     Num::format_bytes
 	 */
-	public static function test_format_bytes($bytes = 0, $decimals = 0)
+	public function test_format_bytes($bytes = 0, $decimals = 0)
 	{
 		$output = Num::format_bytes('204800');
 		$expected = '200 kB';
@@ -48,7 +48,7 @@ class Tests_Num extends TestCase {
 	/**
 	 * @see     Num::quantity
 	 */
-	public static function test_quantity($num = null, $decimals = 0)
+	public function test_quantity($num = null, $decimals = 0)
 	{
 		$output = Num::quantity('7500');
 		$expected = '8K';
@@ -59,7 +59,7 @@ class Tests_Num extends TestCase {
 	/**
 	 * @see     Num::format
 	 */
-	public static function test_format($string = '', $format = '')
+	public function test_format($string = '', $format = '')
 	{
 		$output = Num::format('1234567890', '(000) 000-0000');
 		$expected = '(123) 456-7890';
@@ -70,7 +70,7 @@ class Tests_Num extends TestCase {
 	/**
 	 * @see     Num::mask_string
 	 */
-	public static function test_mask_string($string = '', $format = '', $ignore = ' ')
+	public function test_mask_string($string = '', $format = '', $ignore = ' ')
 	{
 		$output = Num::mask_string('1234567812345678', '**** - **** - **** - 0000', ' -');
 		$expected = '**** - **** - **** - 5678';
@@ -81,7 +81,7 @@ class Tests_Num extends TestCase {
 	/**
 	 * @see     Num::format_phone
 	 */
-	public static function test_format_phone($string = '', $format = '(000) 000-0000')
+	public function test_format_phone($string = '', $format = '(000) 000-0000')
 	{
 		$output = Num::format_phone('1234567890');
 		$expected = '(123) 456-7890';
@@ -92,7 +92,7 @@ class Tests_Num extends TestCase {
 	/**
 	 * @see     Num::smart_format_phone
 	 */
-	public static function test_smart_format_phone($string)
+	public function test_smart_format_phone($string)
 	{
 		$output = Num::smart_phone_format('1234567');
 		$expected = '123-4567';
@@ -103,7 +103,7 @@ class Tests_Num extends TestCase {
 	/**
 	 * @see     Num::format_exp
 	 */
-	public static function test_format_exp($string, $format = '00-00')
+	public function test_format_exp($string, $format = '00-00')
 	{
 		$output = Num::format_exp('1234');
 		$expected = '12-34';
@@ -114,7 +114,7 @@ class Tests_Num extends TestCase {
 	/**
 	 * @see     Num::mask_credit_card
 	 */
-	public static function test_mask_credit_card($string, $format = '**** **** **** 0000')
+	public function test_mask_credit_card($string, $format = '**** **** **** 0000')
 	{
 		$output = Num::mask_credit_card('1234567812345678');
 		$expected = '**** **** **** 5678';
@@ -125,7 +125,7 @@ class Tests_Num extends TestCase {
 	/**
 	 * @see     Num::is_alphanumeric
 	 */
-	public static function test_is_alphanumeric($string)
+	public function test_is_alphanumeric($string)
 	{
 		$output = Num::is_alphanumeric('abc123');
 		$expected = true;
