3f47a7bc3ee69feb2c45e56ebafab2a088bc08f6
Dan Horrigan <dan@dhorrigan.com>
2011-08-27 20:48:24 -0400

    Refactored ViewModel, deprecated a few things, added a bind() method and made it compatible with the newly refactored View class.

--- classes/viewmodel.php
+++ classes/viewmodel.php
@@ -66,8 +66,14 @@ abstract class ViewModel {
 
 	/**
 	 * @var  string|View  view name, after instantiation a View object
+	 * @deprecated until v1.2
 	 */
 	protected $_template;
+
+	/**
+	 * @var  string|View  view name, after instantiation a View object
+	 */
+	protected $_view;
 
 	/**
 	 * @var  bool  whether or not to use auto filtering
@@ -76,23 +82,40 @@ abstract class ViewModel {
 
 	protected function __construct($method, $auto_filter = null)
 	{
-		if (empty($this->_template))
+		// @TODO Remove in 1.2.  This is for backwards compat only.
+		empty($this->_view) and $this->_view = $this->_template;
+
+		if (empty($this->_view))
 		{
+			// Take the class name and guess the view name
 			$class = get_class($this);
-			$this->_template = strtolower(str_replace('_', '/', preg_replace('#^([a-z0-9_]*\\\\)?(View_)?#i', '', $class)));
+			$this->_view = strtolower(str_replace('_', DS, preg_replace('#^([a-z0-9_]*\\\\)?(View_)?#i', '', $class)));
 		}
 
-		$this->set_template();
+		$this->set_view();
+
+		// @TODO Remove in 1.2.  This is for backwards compat only.
+		if ( ! empty($this->_template))
+		{
+			\Log::warning('$this->_template is deprecated.  Please use a $this->_view instead.', __METHOD__);
+			$this->_view = $this->_template;
+		}
+
 		$this->_method = $method;
-		$this->auto_filter = is_null($auto_filter) ? \Config::get('security.auto_filter_view_data', \Config::get('security.auto_encode_view_data', true)) : $auto_filter;
 
 		$this->before();
+	}
 
-		// Set this as the controller output if this is the first ViewModel loaded
-		if (empty(\Request::active()->controller_instance->response->body))
-		{
-			\Request::active()->controller_instance->response->body = $this;
-		}
+	/**
+	 * Must return a View object or something compatible
+	 *
+	 * @return	Object	any object on which the template vars can be set and which has a toString method
+	 * @deprecated until 1.2
+	 */
+	protected function set_template()
+	{
+		\Log::warning('This method is deprecated.  Please use a $this->set_view() instead.', __METHOD__);
+		return $this->set_view();
 	}
 
 	/**
@@ -100,27 +123,9 @@ abstract class ViewModel {
 	 *
 	 * @return	Object	any object on which the template vars can be set and which has a toString method
 	 */
-	protected function set_template()
+	protected function set_view()
 	{
-		$this->_template = \View::forge($this->_template);
-	}
-
-	/**
-	 * Change auto filter setting
-	 *
-	 * @param   null|bool  change setting (bool) or get the current setting (null)
-	 * @return  void|bool  returns current setting or nothing when it is changed
-	 */
-	public function auto_filter($setting = null)
-	{
-		if (func_num_args() == 0)
-		{
-			return $this->auto_filter;
-		}
-
-		$this->auto_filter = (bool) $setting;
-
-		return $this;
+		$this->_view = \View::forge($this->_view);
 	}
 
 	/**
@@ -154,9 +159,13 @@ abstract class ViewModel {
 	 *
 	 * @param	string
 	 */
-	public function get($name)
+	public function &get($key, $default = null)
 	{
-		return $this->_template->{$name};
+		if (is_null($default) and func_num_args() === 1)
+		{
+			return $this->_view->get($key);
+		}
+		return $this->_view->get($key, $default);
 	}
 
 	/**
@@ -165,9 +174,9 @@ abstract class ViewModel {
 	 * @param	string
 	 * @param	mixed
 	 */
-	public function __set($name, $val)
+	public function __set($key, $value)
 	{
-		return $this->set($name, $val);
+		return $this->set($key, $value);
 	}
 
 	/**
@@ -177,12 +186,49 @@ abstract class ViewModel {
 	 * @param	mixed
 	 * @param	bool|null
 	 */
-	public function set($name, $val, $filter = null)
+	public function set($key, $value, $filter = null)
 	{
-		$this->_template->set($name, $val, $filter);
+		$this->_view->set($key, $value, $filter);
 
 		return $this;
 	}
+
+	/**
+	 * Assigns a value by reference. The benefit of binding is that values can
+	 * be altered without re-setting them. It is also possible to bind variables
+	 * before they have values. Assigned values will be available as a
+	 * variable within the view file:
+	 *
+	 *     $this->bind('ref', $bar);
+	 *
+	 * @param   string   variable name
+	 * @param   mixed    referenced variable
+	 * @param   bool     Whether to filter the var on output
+	 * @return  $this
+	 */
+	public function bind($key, &$value, $filter = null)
+	{
+		$this->_view->bind($key, $value, $filter);
+
+		return $this;
+	}
+
+	/**
+	 * Change auto filter setting
+	 *
+	 * @param   null|bool  change setting (bool) or get the current setting (null)
+	 * @return  void|bool  returns current setting or nothing when it is changed
+	 */
+	public function auto_filter($setting = null)
+	{
+		if (func_num_args() == 0)
+		{
+			return $this->_view->auto_filter();
+		}
+
+		return $this->_view->auto_filter($setting);
+	}
+
 
 	/**
 	 * Add variables through method and after() and create template as a string
@@ -192,7 +238,7 @@ abstract class ViewModel {
 		$this->{$this->_method}();
 		$this->after();
 
-		return (string) $this->_template;
+		return (string) $this->_view;
 	}
 
 	/**
