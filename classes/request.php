9c7d60ba0f6789d177580cb24df42e792a266514
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2011-08-31 17:37:06 +0200

    Added deprecation warnings for both empty Controller return and after() methods without output.

--- classes/request.php
+++ classes/request.php
@@ -427,7 +427,17 @@ class Request {
 				if (method_exists($controller, 'after'))
 				{
 					logger(Fuel::L_INFO, 'Calling '.$class.'::after', __METHOD__);
-					$response = $controller->after($response);
+					$response_after = $controller->after($response);
+
+					// @TODO let the after method set the response directly
+					if (is_null($response_after))
+					{
+						logger(\Fuel::L_WARNING, 'The Controller::after() method should accept and return the Controller\'s response, empty return for the after() method is deprecated.', __METHOD__);
+					}
+					else
+					{
+						$response = $response_after;
+					}
 				}
 			}
 			else
@@ -441,11 +451,12 @@ class Request {
 		if (is_null($response))
 		{
 			// @TODO remove this in a future version as we will get rid of it.
-			$this->response =& $controller->response;
+			logger(\Fuel::L_WARNING, 'The Controller should return a string or a Response object, support for the $controller->response object is deprecated.', __METHOD__);
+			$this->response = $controller->response;
 		}
 		elseif ($response instanceof \Response)
 		{
-			$this->response =& $response;
+			$this->response = $response;
 		}
 		else
 		{
