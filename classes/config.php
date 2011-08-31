ce89964c9cfbbb5c727b41ab2148de9ca179f443
WanWizard <wanwizard@wanwizard.eu>
2011-08-31 13:48:35 +0200

    Use Arr::merge() instead of array_merge() in Config to solve numeric key and recursive merge issues

--- classes/config.php
+++ classes/config.php
@@ -37,13 +37,13 @@ class Config {
 			$paths = array_reverse($paths);
 			foreach ($paths as $path)
 			{
-				$config = array_merge($config, \Fuel::load($path));
+				$config = \Arr::merge($config, \Fuel::load($path));
 			}
 		}
 
 		if ($group === null)
 		{
-			static::$items = $reload ? $config : array_merge(static::$items, $config);
+			static::$items = $reload ? $config : \Arr::merge(static::$items, $config);
 		}
 		else
 		{
@@ -52,7 +52,7 @@ class Config {
 			{
 				static::$items[$group] = array();
 			}
-			static::$items[$group] = array_merge(static::$items[$group],$config);
+			static::$items[$group] = \Arr::merge(static::$items[$group],$config);
 		}
 
 		if ( ! is_array($file))
