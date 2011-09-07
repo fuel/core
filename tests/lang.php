200408aea2c8f6d7708cefafda513a08e0d3e1ed
WanWizard <wanwizard@wanwizard.eu>
2011-09-07 20:33:30 +0200

    fixed some unit tests that were broken after recent code changes

--- tests/lang.php
+++ tests/lang.php
@@ -14,58 +14,58 @@ namespace Fuel\Core;
 
 /**
  * Lang class tests
- * 
+ *
  * @group Core
  * @group Lang
  */
 class Test_Lang extends TestCase {
- 
+
 	/**
 	 * Test for Lang::line()
-	 * 
+	 *
 	 * @test
-	 */	
+	 */
 	public function test_line()
 	{
 		Lang::load('test');
-		$output = Lang::line('hello', array('name' => 'Bob'));
+		$output = Lang::get('hello', array('name' => 'Bob'));
 		$expected = 'Hello there Bob!';
 		$this->assertEquals($expected, $output);
 	}
 
 	/**
 	 * Test for Lang::line()
-	 * 
+	 *
 	 * @test
-	 */	
+	 */
 	public function test_line_invalid()
 	{
 		Lang::load('test');
-		$output = Lang::line('non_existant_hello', array('name' => 'Bob'));
-		$expected = 'non_existant_hello';
+		$output = Lang::get('non_existant_hello', array('name' => 'Bob'));
+		$expected = false;
 		$this->assertEquals($expected, $output);
 	}
 
 	/**
 	 * Test for Lang::set()
-	 * 
+	 *
 	 * @test
-	 */	
+	 */
 	public function test_set_return_true()
 	{
 		$output = Lang::set('testing_set_valid', 'Ahoy :name!');
-		$this->assertTrue($output);
+		$this->assertNull($output);
 	}
-	
+
 	/**
 	 * Test for Lang::set()
-	 * 
+	 *
 	 * @test
 	 */
 	public function test_set()
 	{
 		Lang::set('testing_set_valid', 'Ahoy :name!');
-		$output = Lang::line('testing_set_valid', array('name' => 'Bob'));
+		$output = Lang::get('testing_set_valid', array('name' => 'Bob'));
 		$expected = 'Ahoy Bob!';
 		$this->assertEquals($expected, $output);
 	}
