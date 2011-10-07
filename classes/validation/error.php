a360d11db1dbdb373847d584507f8f7ac8f12e58
Phil Lavin <phil@propcom.co.uk>
2011-10-07 13:47:38 +0200

    Coding standards

--- classes/validation/error.php
+++ classes/validation/error.php
@@ -80,8 +80,8 @@ class Validation_Error extends \Exception {
 	 */
 	public function get_message($msg = false, $open = '', $close = '')
 	{
-		$open   = empty($open)?\Config::get('validation.open_single_error', $open):$open;
-		$close  = empty($close)?\Config::get('validation.close_single_error', $close):$close;
+		$open   = empty($open) ? \Config::get('validation.open_single_error', $open) : $open;
+		$close  = empty($close) ? \Config::get('validation.close_single_error', $close) : $close;
 
 		if ($msg === false)
 		{
