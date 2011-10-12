03d393228ad91f09eb71dd74e257ded18e69e782
Daniel Petrie <dan.petrie@cartalyst.com>
2011-10-11 17:31:41 -0700

    heavily modified tasks/migrate and classes/migrate - migrate now works for latest() with individual modules and packages

--- tasks/migrate.php
+++ tasks/migrate.php
@@ -26,10 +26,122 @@ namespace Fuel\Tasks;
 class Migrate
 {
 
+	protected static $default = true;
+	protected static $modules = array();
+	protected static $packages = array();
+	protected static $module_count = 0;
+	protected static $package_count = 0;
+
+	public function __construct()
+	{
+		//load config
+		\Config::load('migrations', true);
+
+		$modules = \Cli::option('modules');
+		$packages = \Cli::option('packages');
+		$default = \Cli::option('default');
+
+		// if modules option set
+		if (!empty($modules))
+		{
+			// if true - get all modules
+			if ($modules === true)
+			{
+				// loop through module paths
+				foreach (\Config::get('module_paths') as $path)
+				{
+					// get all modules that have files in the migration folder
+					foreach (glob($path . '*/') as $m)
+					{
+						if (count(glob($m.\Config::get('migrations.folder').'/*.php')))
+						{
+							static::$modules[] = basename($m);
+						}
+					}
+				}
+			}
+			// else do selected modules
+			else
+			{
+				static::$modules = explode(',', $modules);
+			}
+		}
+
+		// if packages option set
+		if (!empty($packages))
+		{
+			// if true - get all packages
+			if ($packages === true)
+			{
+				// get all packages that have files in the migration folder
+				foreach (glob(PKGPATH . '*/') as $p)
+				{
+					if (count(glob($p.\Config::get('migrations.folder').'/*.php')))
+					{
+						static::$packages[] = basename($p);
+					}
+				}
+			}
+			// else do selected packages
+			else
+			{
+				static::$packages = explode(',', $packages);
+			}
+		}
+
+		if ( (!empty($packages) or !empty($modules)) and empty($default))
+		{
+			static::$default = false;
+		}
+
+		// set count
+		static::$module_count = count(static::$modules);
+		static::$package_count = count(static::$packages);
+	}
+
 	public static function run()
 	{
-		\Config::load('migrations', true);
-		$current_version = \Config::get('migrations.version');
+		// run default migrations if default is true
+		if (static::$default)
+		{
+			static::_run('default');
+		}
+
+		// run modules if passed
+		if (count(static::$modules))
+		{
+			foreach (static::$modules as $module)
+			{
+				static::_run($module, 'module');
+			}
+		}
+
+		//run packages if passed
+		if (count(static::$packages))
+		{
+			foreach (static::$packages as $package)
+			{
+				static::_run($package, 'package');
+			}
+		}
+
+	}
+
+	private static function _run($name, $type = null)
+	{
+		if ($type)
+		{
+			$current_version = \Config::get('migrations.version.'.$type.'.'.$name);
+		}
+		else
+		{
+			$current_version = \Config::get('migrations.version.'.$name);
+		}
+
+		if ( ! $current_version)
+		{
+			$current_version = 0;
+		}
 
 		// -v or --version
 		$version = \Cli::option('v', \Cli::option('version'));
@@ -38,6 +150,13 @@ class Migrate
 		if ($version === true)
 		{
 			\Cli::write('Currently on migration: ' . $current_version .'.', 'green');
+			return;
+		}
+
+		// If version has a value, make sure only 1 item was passed
+		else if ( ! is_null($version) and static::$default + static::$module_count + static::$package_count > 1)
+		{
+			throw new \Oil\Exception('Migration: version only excepts 1 item.');
 			return;
 		}
 
@@ -53,14 +172,14 @@ class Migrate
 		// Specific version
 		if (is_numeric($version) and $version >= 0)
 		{
-			if (\Migrate::version($version) === false)
+			if (\Migrate::version($version, $name, $type) === false)
 			{
 				throw new \Oil\Exception('Migration ' . $version .' could not be found.');
 			}
 
 			else
 			{
-				static::_update_version($version);
+				static::_update_version($version, 'default');
 				\Cli::write('Migrated to version: ' . $version .'.', 'green');
 			}
 		}
@@ -68,14 +187,14 @@ class Migrate
 		// Just go to the latest
 		else
 		{
-			if (($result = \Migrate::latest()) === false)
+			if (($result = \Migrate::latest($name, $type)) === false)
 			{
 				throw new \Oil\Exception("Could not migrate to latest version, still using {$current_version}.");
 			}
 
 			else
 			{
-				static::_update_version($result);
+				static::_update_version($result, 'default');
 				\Cli::write('Migrated to latest version: ' . $result .'.', 'green');
 			}
 		}
@@ -84,12 +203,11 @@ class Migrate
 
 	public static function up()
 	{
-		\Config::load('migrations', true);
-		$version = \Config::get('migrations.version') + 1;
+		$version = \Config::get('migrations.version.default') + 1;
 
 		if ($foo = \Migrate::version($version))
 		{
-			static::_update_version($version);
+			static::_update_version($version, 'default');
 			\Cli::write('Migrated to version: ' . $version .'.', 'green');
 		}
 		else
@@ -100,16 +218,14 @@ class Migrate
 
 	public static function down()
 	{
-		\Config::load('migrations', true);
-
-		if (($version = \Config::get('migrations.version') - 1) < 0)
+		if (($version = \Config::get('migrations.version.default') - 1) < 0)
 		{
 			throw new \Oil\Exception('You are already on the first migration.');
 		}
 
 		if (\Migrate::version($version) !== false)
 		{
-			static::_update_version($version);
+			static::_update_version($version, 'default');
 			\Cli::write("Migrated to version: {$version}.", 'green');
 		}
 		else
@@ -118,6 +234,40 @@ class Migrate
 		}
 	}
 
+	private static function _update_version($version, $name, $type = null)
+	{
+		// if migrations config doesn't exist in app/config
+		if ( ! file_exists($path = APPPATH.'config'.DS.'migrations.php'))
+		{
+			// make sure it exists in core/config and copy to app/config
+			if (file_exists($core_path = COREPATH.'config'.DS.'migrations.php'))
+			{
+				$contents = file_get_contents($core_path);
+			}
+			else
+			{
+				throw new Exception('Config file core/config/migrations.php is missing.');
+				exit;
+			}
+
+			file_put_contents($path, $contents);
+		}
+
+		// set config version
+		if ($type)
+		{
+			\Config::set('migrations.version.'.$type.'.'.$name, $version);
+		}
+		else
+		{
+			\Config::set('migrations.version.'.$name, $version);
+		}
+
+		\Config::save('migrations', 'migrations');
+
+	}
+
+/*
 	private static function _update_version($version)
 	{
 		if (file_exists($path = APPPATH.'config'.DS.'migrations.php'))
@@ -134,11 +284,11 @@ class Migrate
 			exit;
 		}
 
-		$contents = preg_replace("#('version'[ \t]+=>)[ \t]+([0-9]+),#i", "$1 $version,", $contents);
+		$contents = preg_replace("#('default'[ \t]+=>)[ \t]+([0-9]+),#i", "$1 $version,", $contents);
 
 		file_put_contents($path, $contents);
 	}
-
+*/
 	public static function current()
 	{
 		\Migrate::current();
@@ -153,6 +303,13 @@ Usage:
 Fuel options:
     -v, [--version]  # Migrate to a specific version
 
+    # The following disable default migrations unless you add --default to the command
+    --default # re-enables default migration
+    --modules # Migrates all modules
+    --modules=item1,item2 # Migrates specific modules
+    --packages # Migrates all packages
+    --packages=item1,item2 # Migrates specific modules
+
 Description:
     The migrate task can run migrations. You can go up, down or by default go to the current migration marked in the config file.
 
@@ -166,5 +323,6 @@ Examples:
 HELP;
 
 	}
+
 }
 
