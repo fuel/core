05bc586d2c1f755d61846be36cb2a486adf14a29
Frank de Jonge <info@frenky.net>
2011-10-25 16:14:47 +0200

    Fixed typo in core/views/errors/crypt_keys.php

--- views/errors/crypt_keys.php
+++ views/errors/crypt_keys.php
@@ -28,7 +28,7 @@
 		<p class="intro">No write access to APPPATH/config/crypt.php.</p>
 
 		<p>
-			The Fuel crypto functions require a set of unique and truely random crypto keys.
+			The Fuel crypto functions require a set of unique and truelly random crypto keys.
 			These keys are automatically generated and written to the crypto configuration
 			file the first time the application accesses a crypto function.
 		</p>
