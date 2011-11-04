bad4ba7bcd3c1af779b07f5e488f606f0d2c3e14
Dan Horrigan <dan@dhorrigan.com>
2011-11-03 17:49:36 -0700

    Fixes an issue after a prior commit where config's loaded with an extension caused a config not to load.

--- classes/config.php
+++ classes/config.php
@@ -52,7 +52,12 @@ class Config
 		elseif (is_string($file))
 		{
 			$info = pathinfo($file);
-			$type = isset($info['extension']) ? $info['extension'] : 'php';
+			$type = 'php';
+			if (isset($info['extension']))
+			{
+				$type = $info['extension'];
+				$file = substr($file, 0, -(strlen($type) + 1));
+			}
 			$class = '\\Config_'.ucfirst($type);
 
 			if (class_exists($class))
