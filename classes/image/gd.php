b30b5096afc3f8b80f4dc540db6a4f818b45d2fc
kriansa <zdaniel.gp@gmail.com>
2011-11-08 22:24:21 -0200

    Code updated to comply with Fuel's coding standards

--- classes/image/gd.php
+++ classes/image/gd.php
@@ -553,80 +553,80 @@ class Image_Gd extends \Image_Driver
 		
 		if($filename !== null and ! is_writable($filename))
 		{
-			throw new \OutOfBoundsException("You don't have permissions to write to file $filename.");
+			throw new \OutOfBoundsException("You must have write permissions to file $filename.");
 		}
-		
-		// Helper functions to handle with header
-		$int_to_dword = function($n){
-	      return chr($n & 255).chr(($n >> 8) & 255).chr(($n >> 16) & 255).chr(($n >> 24) & 255);
-	    };
-		
-	    $int_to_word = function($n){
-	      return chr($n & 255).chr(($n >> 8) & 255);
-	    };
 		
 		// And here we go
 		
-		$widthOrig = imagesx($img);
+		$original_width = imagesx($img);
 		// width = 16*x
-		$widthFloor = ((floor($widthOrig/16))*16);
-		$widthCeil = ((ceil($widthOrig/16))*16);
+		$floor_width = ((floor($original_width / 16)) * 16);
+		$ceil_width = ((ceil($original_width / 16)) * 16);
 		$height = imagesy($img);
   
-		$size = ($widthCeil*$height*3)+54;
+		$size = ($ceil_width * $height * 3) + 54; // 54 = Header size
   
 		// Bitmap File Header
-		$result = 'BM';     // header (2b)
-		$result .= $int_to_dword($size); // size of file (4b)
-		$result .= $int_to_dword(0); // reserved (4b)
-		$result .= $int_to_dword(54);  // byte location in the file which is first byte of IMAGE (4b)
+		$result = 'BM'.						// header (2b)
+		static::int_to_dword($size).		// size of file (4b)
+		static::int_to_dword(0).			// reserved (4b)
+		static::int_to_dword(54).			// byte location in the file which is first byte of IMAGE (4b)
 		// Bitmap Info Header
-		$result .= $int_to_dword(40);  // Size of BITMAPINFOHEADER (4b)
-		$result .= $int_to_dword($widthCeil);  // width of bitmap (4b)
-		$result .= $int_to_dword($height); // height of bitmap (4b)
-		$result .= $int_to_word(1);  // biPlanes = 1 (2b)
-		$result .= $int_to_word(24); // biBitCount = {1 (mono) or 4 (16 clr ) or 8 (256 clr) or 24 (16 Mil)} (2b)
-		$result .= $int_to_dword(0); // RLE COMPRESSION (4b)
-		$result .= $int_to_dword(0); // width x height (4b)
-		$result .= $int_to_dword(0); // biXPelsPerMeter (4b)
-		$result .= $int_to_dword(0); // biYPelsPerMeter (4b)
-		$result .= $int_to_dword(0); // Number of palettes used (4b)
-		$result .= $int_to_dword(0); // Number of important colour (4b)
+		static::int_to_dword(40).			// Size of BITMAPINFOHEADER (4b)
+		static::int_to_dword($ceil_width).	// width of bitmap (4b)
+		static::int_to_dword($height).		// height of bitmap (4b)
+		static::int_to_word(1).				// biPlanes = 1 (2b)
+		static::int_to_word(24).			// biBitCount = {1 (mono) or 4 (16 clr ) or 8 (256 clr) or 24 (16 Mil)} (2b)
+		static::int_to_dword(0).			// RLE COMPRESSION (4b)
+		static::int_to_dword(0).			// width x height (4b)
+		static::int_to_dword(0).			// biXPelsPerMeter (4b)
+		static::int_to_dword(0).			// biYPelsPerMeter (4b)
+		static::int_to_dword(0).			// Number of palettes used (4b)
+		static::int_to_dword(0);			// Number of important colour (4b)
 		
-		// is faster than chr()
-		$arrChr = array();
-		for($i=0; $i<256; $i++){
-			$arrChr[$i] = chr($i);
+		// Buffer for chr()
+		$chr = array();
+		for($i = 0; $i < 256; $i++){
+			$chr[$i] = chr($i);
 		}
   
-		// creates image data
-		$bgfillcolor = array("red"=>0, "green"=>0, "blue"=>0);
-  
-		// bottom to top - left to right - attention blue green red !!!
-		$y=$height-1;
-		for ($y2=0; $y2<$height; $y2++) {
-			for ($x=0; $x<$widthFloor;  ) {
-				$rgb = imagecolorsforindex($img, imagecolorat($img, $x++, $y));
-				$result .= $arrChr[$rgb["blue"]].$arrChr[$rgb["green"]].$arrChr[$rgb["red"]];
-				$rgb = imagecolorsforindex($img, imagecolorat($img, $x++, $y));
-				$result .= $arrChr[$rgb["blue"]].$arrChr[$rgb["green"]].$arrChr[$rgb["red"]];
-				$rgb = imagecolorsforindex($img, imagecolorat($img, $x++, $y));
-				$result .= $arrChr[$rgb["blue"]].$arrChr[$rgb["green"]].$arrChr[$rgb["red"]];
-				$rgb = imagecolorsforindex($img, imagecolorat($img, $x++, $y));
-				$result .= $arrChr[$rgb["blue"]].$arrChr[$rgb["green"]].$arrChr[$rgb["red"]];
-				$rgb = imagecolorsforindex($img, imagecolorat($img, $x++, $y));
-				$result .= $arrChr[$rgb["blue"]].$arrChr[$rgb["green"]].$arrChr[$rgb["red"]];
-				$rgb = imagecolorsforindex($img, imagecolorat($img, $x++, $y));
-				$result .= $arrChr[$rgb["blue"]].$arrChr[$rgb["green"]].$arrChr[$rgb["red"]];
-				$rgb = imagecolorsforindex($img, imagecolorat($img, $x++, $y));
-				$result .= $arrChr[$rgb["blue"]].$arrChr[$rgb["green"]].$arrChr[$rgb["red"]];
-				$rgb = imagecolorsforindex($img, imagecolorat($img, $x++, $y));
-				$result .= $arrChr[$rgb["blue"]].$arrChr[$rgb["green"]].$arrChr[$rgb["red"]];
+		// creates image background (default: white)
+		$bgfillcolor = array(
+			"red"   => 255,
+			"green" => 255,
+			"blue"  => 255,
+		);
+
+		// Bottom to top - left to right - attention blue green red !!!
+		$y = $height - 1;
+		for ($y2 = 0; $y2 < $height; $y2++)
+		{
+			for ($x = 0; $x < $floor_width;  )
+			{
+				$rgb     = imagecolorsforindex($img, imagecolorat($img, $x++, $y));
+				$result .= $chr[$rgb["blue"]].$chr[$rgb["green"]].$chr[$rgb["red"]];
+				$rgb     = imagecolorsforindex($img, imagecolorat($img, $x++, $y));
+				$result .= $chr[$rgb["blue"]].$chr[$rgb["green"]].$chr[$rgb["red"]];
+				$rgb     = imagecolorsforindex($img, imagecolorat($img, $x++, $y));
+				$result .= $chr[$rgb["blue"]].$chr[$rgb["green"]].$chr[$rgb["red"]];
+				$rgb     = imagecolorsforindex($img, imagecolorat($img, $x++, $y));
+				$result .= $chr[$rgb["blue"]].$chr[$rgb["green"]].$chr[$rgb["red"]];
+				$rgb     = imagecolorsforindex($img, imagecolorat($img, $x++, $y));
+				$result .= $chr[$rgb["blue"]].$chr[$rgb["green"]].$chr[$rgb["red"]];
+				$rgb     = imagecolorsforindex($img, imagecolorat($img, $x++, $y));
+				$result .= $chr[$rgb["blue"]].$chr[$rgb["green"]].$chr[$rgb["red"]];
+				$rgb     = imagecolorsforindex($img, imagecolorat($img, $x++, $y));
+				$result .= $chr[$rgb["blue"]].$chr[$rgb["green"]].$chr[$rgb["red"]];
+				$rgb     = imagecolorsforindex($img, imagecolorat($img, $x++, $y));
+				$result .= $chr[$rgb["blue"]].$chr[$rgb["green"]].$chr[$rgb["red"]];
 			}
-			for ($x=$widthFloor; $x<$widthCeil; $x++) {
-				$rgb = ($x<$widthOrig) ? imagecolorsforindex($img, imagecolorat($img, $x, $y)) : $bgfillcolor;
-				$result .= $arrChr[$rgb["blue"]].$arrChr[$rgb["green"]].$arrChr[$rgb["red"]];
+
+			for ($x = $floor_width; $x < $ceil_width; $x++)
+			{
+				$rgb     = ($x < $original_width) ? imagecolorsforindex($img, imagecolorat($img, $x, $y)) : $bgfillcolor;
+				$result .= $chr[$rgb["blue"]].$chr[$rgb["green"]].$chr[$rgb["red"]];
 			}
+
 			$y--;
 		}
 
@@ -638,6 +638,28 @@ class Image_Gd extends \Image_Driver
 		{
 			file_put_contents($filename, $result);
 		}
+	}
+	
+	/**
+	 * Helper for imagebmp
+	 * 
+	 * @param		int			Integer
+	 * @return		string		DWORD integer format
+	 */
+	protected static function int_to_dword($n)
+	{
+		return chr($n & 255).chr(($n >> 8) & 255).chr(($n >> 16) & 255).chr(($n >> 24) & 255);
+	}
+
+	/**
+	 * Helper for imagebmp
+	 * 
+	 * @param		int			Integer
+	 * @return		string		WORD integer format
+	 */
+	protected static function int_to_word($n)
+	{
+		return chr($n & 255).chr(($n >> 8) & 255);
 	}
 
 	/**
@@ -655,37 +677,40 @@ class Image_Gd extends \Image_Driver
 		{
 			throw new \OutOfBoundsException("Image file $src does not exist.");
 		}
-	
+
 		// Get the headers
 		extract(unpack('vtype/Vsize/Vreserved/Voffset/Vsize/Vwidth/Vheight/vplanes/vbits/Vcompression/Vimagesize/Vxres/Vyres/Vcolor/Vimportant', fread($srch, 54)));
-	
+
 		// Check for BMP signature
 		if($type != 0x4D42)
 		{
 			throw new \InvalidArgumentException('Source image is not a BMP.');
 		}
-		
+
 		// Create the temp file to handle
 		$tmpfile = tempnam(APPPATH.'tmp'.DS, uniqid('bmp2gd'));
 		$fp = fopen($tmpfile, 'wb');
-	
+
 		// Set the pallete
 		$palette_size = $offset - 54;
 		$ncolor = $palette_size / 4;
-	
+
 		// True-color vs. palette
-		$gd_header = ($palette_size == 0)?"\xFF\xFE":"\xFF\xFF";
+		$gd_header = ($palette_size == 0) ? "\xFF\xFE" : "\xFF\xFF";
 		$gd_header .= pack('n2', $width, $height);
-		$gd_header .= ($palette_size == 0)?"\x01":"\x00";
-		if ($palette_size) { $gd_header .= pack('n', $ncolor); }
-		
+		$gd_header .= ($palette_size == 0) ? "\x01" : "\x00";
+		if ($palette_size)
+		{
+			$gd_header .= pack('n', $ncolor);
+		}
+
 		// Do not allow transparency
 		$gd_header .= "\xFF\xFF\xFF\xFF";
-		
+
 		// Write the destination headers
 		fwrite($fp, $gd_header);
 		unset($gd_header);
-	
+
 		// if we have a palette
 		if ($palette_size) {
 			// read the palette
@@ -694,7 +719,8 @@ class Image_Gd extends \Image_Driver
 			$gd_palette = '';
 			$j = 0;
 			// loop of the palette
-			while ($j < $palette_size) {
+			while ($j < $palette_size)
+			{
 				$b = $palette{$j++};
 				$g = $palette{$j++};
 				$r = $palette{$j++};
@@ -708,30 +734,38 @@ class Image_Gd extends \Image_Driver
 			fwrite($fp, $gd_palette);
 			unset($gd_palette);
 		}
-	
+
 		// scan line size and alignment
 		$scan_line_size = (($bits * $width) + 7) >> 3;
-		$scan_line_align = ($scan_line_size & 0x03) ? 4 - ($scan_line_size & 0x03):0;
-	
+		$scan_line_align = ($scan_line_size & 0x03) ? 4 - ($scan_line_size & 0x03) : 0;
+
 		// main loop
-		for ($i = 0, $l = $height - 1; $i < $height; $i++, $l--) {
+		for ($i = 0, $l = $height - 1; $i < $height; $i++, $l--)
+		{
 			// create scan lines starting from bottom
 			fseek($srch, $offset + (($scan_line_size + $scan_line_align) * $l));
 			$scan_line = fread($srch, $scan_line_size);
 			$gd_scan_line = '';
-			if($bits == 24) {
+			if($bits == 24)
+			{
 				$j = 0;
-				while($j < $scan_line_size) {
+				while($j < $scan_line_size)
+				{
 					$b = $scan_line{$j++};
 					$g = $scan_line{$j++};
 					$r = $scan_line{$j++};
 					$gd_scan_line .= "\x00".$r.$g.$b;
 				}
 			}
-			elseif($bits == 8) { $gd_scan_line = $scan_line; }
-			elseif($bits == 4) {
+			elseif($bits == 8)
+			{
+				$gd_scan_line = $scan_line;
+			}
+			elseif($bits == 4)
+			{
 				$j = 0;
-				while($j < $scan_line_size) {
+				while($j < $scan_line_size)
+				{
 					$byte = ord($scan_line{$j++});
 					$p1 = chr($byte >> 4);
 					$p2 = chr($byte & 0x0F);
@@ -739,20 +773,22 @@ class Image_Gd extends \Image_Driver
 				}
 				$gd_scan_line = substr($gd_scan_line, 0, $width);
 			}
-			elseif($bits == 1) {
-			  $j = 0;
-			  while($j < $scan_line_size) {
-				  $byte = ord($scan_line{$j++});
-				  $p1 = chr((int) (($byte & 0x80) != 0));
-				  $p2 = chr((int) (($byte & 0x40) != 0));
-				  $p3 = chr((int) (($byte & 0x20) != 0));
-				  $p4 = chr((int) (($byte & 0x10) != 0));
-				  $p5 = chr((int) (($byte & 0x08) != 0));
-				  $p6 = chr((int) (($byte & 0x04) != 0));
-				  $p7 = chr((int) (($byte & 0x02) != 0));
-				  $p8 = chr((int) (($byte & 0x01) != 0));
-				  $gd_scan_line .= $p1.$p2.$p3.$p4.$p5.$p6.$p7.$p8;
-			  }
+			elseif($bits == 1)
+			{
+				$j = 0;
+				while($j < $scan_line_size)
+				{
+					$byte = ord($scan_line{$j++});
+					$p1 = chr((int) (($byte & 0x80) != 0));
+					$p2 = chr((int) (($byte & 0x40) != 0));
+					$p3 = chr((int) (($byte & 0x20) != 0));
+					$p4 = chr((int) (($byte & 0x10) != 0));
+					$p5 = chr((int) (($byte & 0x08) != 0));
+					$p6 = chr((int) (($byte & 0x04) != 0));
+					$p7 = chr((int) (($byte & 0x02) != 0));
+					$p8 = chr((int) (($byte & 0x01) != 0));
+					$gd_scan_line .= $p1.$p2.$p3.$p4.$p5.$p6.$p7.$p8;
+				}
 				// put the gd scan lines together
 				$gd_scan_line = substr($gd_scan_line, 0, $width);
 			}
@@ -762,10 +798,10 @@ class Image_Gd extends \Image_Driver
 		}
 		// close the source file
 		fclose($fp);
-		
+
 		$gd = imagecreatefromgd($tmpfile);
 		unlink($tmpfile);
-		
+
 		return $gd;
 	}
 }
