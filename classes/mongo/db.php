4d2ec51a4d81409b05c013ac18a9cf99cb20a8ed
Isern Palaus <ipalaus@ipalaus.es>
2011-11-09 14:45:59 +0100

    Ensure that the variables are strings and prevents of breaking the query with an array.

--- classes/mongo/db.php
+++ classes/mongo/db.php
@@ -297,7 +297,7 @@ class Mongo_Db
 	{
 		foreach ($wheres as $wh => $val)
 		{
-			$this->wheres[$wh] = $val;
+			$this->wheres[$wh] = (string) $val;
 		}
 		return $this;
 	}
@@ -319,7 +319,7 @@ class Mongo_Db
 
 			foreach ($wheres as $wh => $val)
 			{
-				$this->wheres['$or'][] = array($wh=>$val);
+				$this->wheres['$or'][] = array($wh => (string) $val);
 			}
 		}
 		return $this;
