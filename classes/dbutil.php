2f4fd92cf92f8b65e2ba657d57ab1147f76beb14
Frank de Jonge <info@frenky.net>
2011-11-17 17:09:21 +1100

    Fixes #672: NOT NULL was default before the small rewrite and it is once more. Simplified the default value parsing.

--- classes/dbutil.php
+++ classes/dbutil.php
@@ -287,15 +287,18 @@ class DBUtil
 				$sql .= ' UNSIGNED';
 			}
 
-			$sql .= array_key_exists('DEFAULT', $attr) ? ' DEFAULT '. (($attr['DEFAULT'] instanceof \Database_Expression) ? $attr['DEFAULT']  : \DB::escape($attr['DEFAULT'])) : '';
-
-			if(array_key_exists('NULL', $attr))
+			if(array_key_exists('DEFAULT', $attr))
 			{
-				$sql .= $attr['NULL'] === true ? ' NULL' : ' NOT NULL';
+				$sql .= ' DEFAULT '.(($attr['DEFAULT'] instanceof \Database_Expression) ? $attr['DEFAULT']  : \DB::escape($attr['DEFAULT']));
 			}
-			elseif( ! array_key_exists('DEFAULT', $attr))
+
+			if(array_key_exists('NULL', $attr) and $attr['NULL'] === true)
 			{
 				$sql .= ' NULL';
+			}
+			else
+			{
+				$sql .= ' NOT NULL';
 			}
 
 			if (array_key_exists('AUTO_INCREMENT', $attr) and $attr['AUTO_INCREMENT'] === true)
