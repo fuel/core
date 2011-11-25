eeab3e7790fb7169cfcb8ab56929698bd5bbf6fb
Joshua Rubin <jrubin@zvelo.com>
2011-11-25 00:17:30 -0700

    prevent some warnings
    Signed-off-by: Joshua Rubin <jrubin@zvelo.com>

--- classes/controller/rest.php
+++ classes/controller/rest.php
@@ -249,7 +249,7 @@ abstract class Controller_Rest extends \Controller
 			return false;
 		}
 
-		$valid_logins = & \Config::get('rest.valid_logins');
+		$valid_logins = \Config::get('rest.valid_logins');
 
 		if (!array_key_exists($username, $valid_logins))
 		{
@@ -327,7 +327,7 @@ abstract class Controller_Rest extends \Controller
 			static::_force_login($uniqid);
 		}
 
-		$valid_logins = & \Config::get('rest.valid_logins');
+		$valid_logins = \Config::get('rest.valid_logins');
 		$valid_pass = $valid_logins[$digest['username']];
 
 		// This is the valid response expected
