fd936da5e42020cc7343c538681f83d75689a6ff
WanWizard <wanwizard@wanwizard.eu>
2011-12-15 17:21:30 +0100

    made profiler methods NOP's when the profiler is not loaded. reverted #727
    also fixed the issue mentioned in #727, which was caused by the log class accessing
    the profilers Console class, before Fuel::init() has initialized the profiler

--- classes/profiler.php
+++ classes/profiler.php
@@ -2,6 +2,7 @@
 
 namespace Fuel\Core;
 
+import('phpquickprofiler/console', 'vendor');
 import('phpquickprofiler/phpquickprofiler', 'vendor');
 
 use \Console;
@@ -14,43 +15,34 @@ class Profiler
 
 	protected static $query = null;
 
-	protected static $initialized = false;
-
-	public static function _init()
-	{
-		static::init();
-	}
-
 	public static function init()
 	{
-		if (static::$initialized)
+		if ( ! static::$profiler)
 		{
-			return;
+			static::$profiler = new PhpQuickProfiler(FUEL_START_TIME);
+			static::$profiler->queries = array();
+			static::$profiler->queryCount = 0;
 		}
-		static::$profiler = new PhpQuickProfiler(FUEL_START_TIME);
-		static::$profiler->queries = array();
-		static::$profiler->queryCount = 0;
-		static::$initialized = true;
 	}
 
 	public static function mark($label)
 	{
-		Console::logSpeed($label);
+		static::$profiler and Console::logSpeed($label);
 	}
 
 	public static function mark_memory($var = false, $name = 'PHP')
 	{
-		Console::logMemory($var, $name);
+		static::$profiler and Console::logMemory($var, $name);
 	}
 
 	public static function console($text)
 	{
-		Console::log($text);
+		static::$profiler or Console::log($text);
 	}
 
 	public static function output()
 	{
-		return static::$profiler->display(static::$profiler);
+		return static::$profiler ? static::$profiler->display(static::$profiler) : '';
 	}
 
 	public static function start($dbname, $sql)
@@ -67,9 +59,12 @@ class Profiler
 
 	public static function stop($text)
 	{
-		static::$query['time'] = (static::$profiler->getMicroTime() - static::$query['time']) *1000;
-		array_push(static::$profiler->queries, static::$query);
-		static::$profiler->queryCount++;
+		if (static::$profiler)
+		{
+			static::$query['time'] = (static::$profiler->getMicroTime() - static::$query['time']) *1000;
+			array_push(static::$profiler->queries, static::$query);
+			static::$profiler->queryCount++;
+		}
 	}
 
 	public static function delete($text)
