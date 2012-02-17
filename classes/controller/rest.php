33e601c92cc1990b243f4059c0359b064e9cba4a
Dan Harper <intouch@danharper.me>
2012-02-17 16:51:48 +0000

    PHP documentation and structure

--- classes/controller/rest.php
+++ classes/controller/rest.php
@@ -44,38 +44,18 @@ abstract class Controller_Rest extends \Controller
 		'csv' => 'application/csv'
 	);
 
+	/**
+	 * @var  array  contains a list of model fields which can be exposed on an API
+	 */
 	protected $_exposable = array();
 
+	/**
+	 * Exposable computed/dynamic model fields
+	 * 
+	 * @param  Model $model The current model to computed exposable fields on
+	 * @return array computed/dynamic fields to expose
+	 */
 	protected function _exposable($model) {}
-
-	public function expose($models)
-	{
-		$exposed = array();
-		if (is_array($models))
-		{
-			foreach ($models as $model)
-			{
-				$exposed[] = $this->_expose_one($model);
-			}
-		}
-		else {
-			$exposed[] = $this->_expose_one($models);
-		}
-		return $exposed;
-	}
-
-	protected function _expose_one($model)
-	{
-		$exposed = array();
-		foreach ($this->_exposable as $key)
-		{
-			$exposed[$key] = $model->$key;
-		}
-		$dynamic = $this->_exposable($model);
-		if (is_array($dynamic))
-			return array_merge($exposed, $dynamic);
-		return $exposed;
-	}
 
 	public function before()
 	{
@@ -305,6 +285,44 @@ abstract class Controller_Rest extends \Controller
 
 		// Nope, just return the string
 		return $lang;
+	}
+
+	/**
+	 * Expose
+	 * 
+	 * Get exposable data for model(s) provided. Will expose fields specified in the
+	 * $_exposable array and any computed fields returned in an array from the _exposable()
+	 * function
+	 * @param  array/model $models Model(s) to expose
+	 * @return array
+	 */
+	public function expose($models)
+	{
+		$exposed = array();
+		if (is_array($models))
+		{
+			foreach ($models as $model)
+			{
+				$exposed[] = $this->_expose_one($model);
+			}
+		}
+		else {
+			$exposed[] = $this->_expose_one($models);
+		}
+		return $exposed;
+	}
+
+	protected function _expose_one($model)
+	{
+		$exposed = array();
+		foreach ($this->_exposable as $key)
+		{
+			$exposed[$key] = $model->$key;
+		}
+		$dynamic = $this->_exposable($model);
+		if (is_array($dynamic))
+			return array_merge($exposed, $dynamic);
+		return $exposed;
 	}
 
 	// SECURITY FUNCTIONS ---------------------------------------------------------
