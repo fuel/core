b4eca1eb1e55f3b6651781b54bce8388b7870135
Frank de Jonge <info@frenky.net>
2012-03-23 14:18:41 +0100

    Added mysql_date to date config.
    Signed-off-by: Frank de Jonge <info@frenky.net>

--- config/date.php
+++ config/date.php
@@ -26,22 +26,21 @@ return array(
 	 * A couple of named patterns that are often used
 	 */
 	'patterns' => array(
-		'local'		=> '%c',
+		'local'		 => '%c',
 
-		'mysql'		=> '%Y-%m-%d %H:%M:%S',
+		'mysql'		 => '%Y-%m-%d %H:%M:%S',
+		'mysql_date' => '%Y-%m-%d',
 
-		'us'		=> '%m/%d/%Y',
-		'us_short'	=> '%m/%d',
-		'us_named'	=> '%B %d %Y',
-		'us_full'	=> '%I:%M %p, %B %d %Y',
-		'eu'		=> '%d/%m/%Y',
-		'eu_short'	=> '%d/%m',
-		'eu_named'	=> '%d %B %Y',
-		'eu_full'	=> '%H:%M, %d %B %Y',
+		'us'		 => '%m/%d/%Y',
+		'us_short'	 => '%m/%d',
+		'us_named'	 => '%B %d %Y',
+		'us_full'	 => '%I:%M %p, %B %d %Y',
+		'eu'		 => '%d/%m/%Y',
+		'eu_short'	 => '%d/%m',
+		'eu_named'	 => '%d %B %Y',
+		'eu_full'	 => '%H:%M, %d %B %Y',
 
-		'24h'		=> '%H:%M',
-		'12h'		=> '%I:%M %p'
+		'24h'		 => '%H:%M',
+		'12h'		 => '%I:%M %p'
 	)
 );
-
-
