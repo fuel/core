5a0cbb3a0361aa64009405e86bb4b48422ee57d8
WanWizard <wanwizard@wanwizard.eu>
2012-04-19 22:13:27 +0200

    added outofbounds check on Date::create_from_string()
    this will print an error on 32-bit systems that pass dates outside the
    1901-2038 year range...

--- classes/date.php
+++ classes/date.php
@@ -170,8 +170,13 @@ class Date
 		{
 			throw new \UnexpectedValueException('Input was not recognized by pattern.');
 		}
+
 		$timestamp = mktime($time['tm_hour'], $time['tm_min'], $time['tm_sec'],
 						$time['tm_mon'] + 1, $time['tm_mday'], $time['tm_year'] + 1900);
+		if ($timestamp === false)
+		{
+			throw new \OutOfBoundsException('Input was invalid.'.(PHP_INT_SIZE == 4?' A 32-bit system only supports dates between 1901 and 2038.':''));
+		}
 
 		return static::forge($timestamp);
 	}
