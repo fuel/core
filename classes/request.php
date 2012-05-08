69fa67e97afa7edcfc9df60494278eb8d6b0bb6a
WanWizard <wanwizard@wanwizard.eu>
2012-05-08 13:04:56 +0200

    Fixed request logging no longer shows the requested URI

--- classes/request.php
+++ classes/request.php
@@ -261,7 +261,7 @@ class Request
 		$this->uri = new \Uri($uri);
 		$this->method = $method;
 
-		logger(\Fuel::L_INFO, 'Creating a new Request with URI = "'.$uri.'"', __METHOD__);
+		logger(\Fuel::L_INFO, 'Creating a new Request with URI = "'.$this->uri->get().'"', __METHOD__);
 
 		// check if a module was requested
 		if (count($this->uri->segments()) and $module_path = \Module::exists($this->uri->get_segment(0)))
