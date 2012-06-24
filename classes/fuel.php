faff5ad4c0e607ebf6559c2596a18f8926291ea6
WanWizard <wanwizard@wanwizard.eu>
2012-06-24 22:46:42 +0200

    changed the FuelPHP version to 1.2.1

--- classes/fuel.php
+++ classes/fuel.php
@@ -29,7 +29,7 @@ class Fuel
 	/**
 	 * @var  string  The version of Fuel
 	 */
-	const VERSION = '1.2.1-dev';
+	const VERSION = '1.2.1';
 
 	/**
 	 * @var  string  constant used for when in testing mode
