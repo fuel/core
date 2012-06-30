0f4267817538654d1e1c8e70fff07dc345ec7b3c
Fumito Mizuno <ounziw@gmail.com>
2012-07-01 08:43:57 +0900

    some unittest for pagination.php

--- tests/pagination.php
+++ tests/pagination.php
@@ -20,5 +20,62 @@ namespace Fuel\Core;
  */
 class Test_Pagination extends TestCase
 {
- 	public function test_foo() {}
+	public function setup() 
+	{
+	Pagination::set_config(array(
+		'pagination_url' => 'http://docs.fuelphp.com/',
+		'uri_segment' => 2,
+		'total_items' => 10,
+		'per_page' => 5,
+	));
+	}
+
+	/**
+	 * first page: previous inactive
+	 * 
+	 */
+	public function test_previouslink_inactive() 
+	{
+	Pagination::set_config(array(
+		'current_page' => 1,
+	));
+		$output = Pagination::prev_link('prev');
+		$expected = ' <span class="previous-inactive">&laquo; prev </span>';
+		$this->assertEquals($expected, $output);
+	}
+
+	public function test_previouslink_active() 
+	{
+	Pagination::set_config(array(
+		'current_page' => 2,
+	));
+		$output = Pagination::prev_link('prev');
+		$expected = '<span class="previous"> <a href="http://docs.fuelphp.com">&laquo; prev</a> </span>';
+		$this->assertEquals($expected, $output);
+	}
+
+	public function test_nextlink_active() 
+	{
+	Pagination::set_config(array(
+		'current_page' => 1,
+	));
+		$output = Pagination::next_link('next');
+		$expected = '<span class="next"> <a href="http://docs.fuelphp.com/2">next &raquo;</a> </span>';
+		$this->assertEquals($expected, $output);
+	}
+
+	/**
+	 * last page: next inactive
+	 * 
+	 */
+	public function test_nextlink_inactive() 
+	{
+	Pagination::set_config(array(
+		'current_page' => 2,
+	));
+		$output = Pagination::next_link('next');
+		$expected = ' <span class="next-inactive">next &raquo; </span>';
+		$this->assertEquals($expected, $output);
+	}
+
 }
