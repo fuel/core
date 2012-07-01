a82603aedc14c2b08203c31823eedc3b0c2554ab
Fumito Mizuno <ounziw@gmail.com>
2012-07-01 18:49:22 +0900

    indent fixed

--- tests/pagination.php
+++ tests/pagination.php
@@ -22,12 +22,12 @@ class Test_Pagination extends TestCase
 {
 	public function setup() 
 	{
-	Pagination::set_config(array(
-		'pagination_url' => 'http://docs.fuelphp.com/',
-		'uri_segment' => 2,
-		'total_items' => 10,
-		'per_page' => 5,
-	));
+		Pagination::set_config(array(
+			'pagination_url' => 'http://docs.fuelphp.com/',
+			'uri_segment' => 2,
+			'total_items' => 10,
+			'per_page' => 5,
+		));
 	}
 
 	/**
@@ -36,9 +36,9 @@ class Test_Pagination extends TestCase
 	 */
 	public function test_previouslink_inactive() 
 	{
-	Pagination::set_config(array(
-		'current_page' => 1,
-	));
+		Pagination::set_config(array(
+			'current_page' => 1,
+		));
 		$output = Pagination::prev_link('prev');
 		$expected = ' <span class="previous-inactive">&laquo; prev </span>';
 		$this->assertEquals($expected, $output);
@@ -46,9 +46,9 @@ class Test_Pagination extends TestCase
 
 	public function test_previouslink_active() 
 	{
-	Pagination::set_config(array(
-		'current_page' => 2,
-	));
+		Pagination::set_config(array(
+			'current_page' => 2,
+		));
 		$output = Pagination::prev_link('prev');
 		$expected = '<span class="previous"> <a href="http://docs.fuelphp.com">&laquo; prev</a> </span>';
 		$this->assertEquals($expected, $output);
@@ -56,9 +56,9 @@ class Test_Pagination extends TestCase
 
 	public function test_nextlink_active() 
 	{
-	Pagination::set_config(array(
-		'current_page' => 1,
-	));
+		Pagination::set_config(array(
+			'current_page' => 1,
+		));
 		$output = Pagination::next_link('next');
 		$expected = '<span class="next"> <a href="http://docs.fuelphp.com/2">next &raquo;</a> </span>';
 		$this->assertEquals($expected, $output);
@@ -70,9 +70,9 @@ class Test_Pagination extends TestCase
 	 */
 	public function test_nextlink_inactive() 
 	{
-	Pagination::set_config(array(
-		'current_page' => 2,
-	));
+		Pagination::set_config(array(
+			'current_page' => 2,
+		));
 		$output = Pagination::next_link('next');
 		$expected = ' <span class="next-inactive">next &raquo; </span>';
 		$this->assertEquals($expected, $output);
