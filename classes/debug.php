345c8c4b94422219ecb4c10ad8184c32d331b6bb
WanWizard <wanwizard@wanwizard.eu>
2012-07-05 16:15:52 +0200

    disable fancy Debug::dump output when in CLI mode

--- classes/debug.php
+++ classes/debug.php
@@ -39,45 +39,56 @@ class Debug
 	 */
 	public static function dump()
 	{
-		$backtrace = debug_backtrace();
-
-		// If being called from within, show the file above in the backtrack
-		if (strpos($backtrace[0]['file'], 'core/classes/debug.php') !== FALSE)
+		if (\Fuel::$is_cli)
 		{
-			$callee = $backtrace[1];
-			$label = \Inflector::humanize($backtrace[1]['function']);
+			// no fancy flying, jump dump 'm
+			foreach (func_get_args() as $arg)
+			{
+				var_dump($arg);
+			}
 		}
 		else
 		{
-			$callee = $backtrace[0];
-			$label = 'Debug';
-		}
+			$backtrace = debug_backtrace();
 
-		$arguments = func_get_args();
+			// If being called from within, show the file above in the backtrack
+			if (strpos($backtrace[0]['file'], 'core/classes/debug.php') !== FALSE)
+			{
+				$callee = $backtrace[1];
+				$label = \Inflector::humanize($backtrace[1]['function']);
+			}
+			else
+			{
+				$callee = $backtrace[0];
+				$label = 'Debug';
+			}
 
-		$callee['file'] = \Fuel::clean_path($callee['file']);
+			$arguments = func_get_args();
 
-		if ( ! static::$js_displayed)
-		{
-			echo <<<JS
-<script type="text/javascript">function fuel_debug_toggle(a){if(document.getElementById){if(document.getElementById(a).style.display=="none"){document.getElementById(a).style.display="block"}else{document.getElementById(a).style.display="none"}}else{if(document.layers){if(document.id.display=="none"){document.id.display="block"}else{document.id.display="none"}}else{if(document.all.id.style.display=="none"){document.all.id.style.display="block"}else{document.all.id.style.display="none"}}}};</script>
+			$callee['file'] = \Fuel::clean_path($callee['file']);
+
+			if ( ! static::$js_displayed)
+			{
+				echo <<<JS
+	<script type="text/javascript">function fuel_debug_toggle(a){if(document.getElementById){if(document.getElementById(a).style.display=="none"){document.getElementById(a).style.display="block"}else{document.getElementById(a).style.display="none"}}else{if(document.layers){if(document.id.display=="none"){document.id.display="block"}else{document.id.display="none"}}else{if(document.all.id.style.display=="none"){document.all.id.style.display="block"}else{document.all.id.style.display="none"}}}};</script>
 JS;
-			static::$js_displayed = true;
-		}
-		echo '<div style="font-size: 13px;background: #EEE !important; border:1px solid #666; color: #000 !important; padding:10px;">';
-		echo '<h1 style="border-bottom: 1px solid #CCC; padding: 0 0 5px 0; margin: 0 0 5px 0; font: bold 120% sans-serif;">'.$callee['file'].' @ line: '.$callee['line'].'</h1>';
-		echo '<pre style="overflow:auto;font-size:100%;">';
+				static::$js_displayed = true;
+			}
+			echo '<div style="font-size: 13px;background: #EEE !important; border:1px solid #666; color: #000 !important; padding:10px;">';
+			echo '<h1 style="border-bottom: 1px solid #CCC; padding: 0 0 5px 0; margin: 0 0 5px 0; font: bold 120% sans-serif;">'.$callee['file'].' @ line: '.$callee['line'].'</h1>';
+			echo '<pre style="overflow:auto;font-size:100%;">';
 
-		$count = count($arguments);
-		for ($i = 1; $i <= $count; $i++)
-		{
-			echo '<strong>Variable #'.$i.':</strong>'.PHP_EOL;
-			echo static::format('', $arguments[$i - 1]);
-			echo PHP_EOL.PHP_EOL;
-		}
+			$count = count($arguments);
+			for ($i = 1; $i <= $count; $i++)
+			{
+				echo '<strong>Variable #'.$i.':</strong>'.PHP_EOL;
+				echo static::format('', $arguments[$i - 1]);
+				echo PHP_EOL.PHP_EOL;
+			}
 
-		echo "</pre>";
-		echo "</div>";
+			echo "</pre>";
+			echo "</div>";
+		}
 	}
 
 	/**
