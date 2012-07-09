7474f666d6ca0f7bc46fad9b4d989e6218f51631
WanWizard <wanwizard@wanwizard.eu>
2012-07-09 12:59:44 +0200

    Added missing pbkdf2() method to PHPSecLib/Crypt/Hash

--- vendor/phpseclib/Crypt/Hash.php
+++ vendor/phpseclib/Crypt/Hash.php
@@ -347,6 +347,41 @@ class Crypt_Hash {
         return $this->l;
     }
 
+	/* PBKDF2 Implementation (described in RFC 2898)
+	 *
+	 *  @param string p password
+	 *  @param string s salt
+	 *  @param int c iteration count (use 1000 or higher)
+	 *  @param int kl derived key length
+	 *  @param string a hash algorithm
+	 *
+	 *  @return string derived key
+	 */
+	public function pbkdf2( $p, $s, $c, $kl, $a = 'sha256' )
+	{
+		$hl = strlen(hash($a, null, true)); # Hash length
+		$kb = ceil($kl / $hl);              # Key blocks to compute
+		$dk = '';                           # Derived key
+
+		# Create key
+		for ( $block = 1; $block <= $kb; $block ++ )
+		{
+			# Initial hash for this block
+			$ib = $b = hash_hmac($a, $s . pack('N', $block), $p, true);
+
+			# Perform block iterations
+			for ( $i = 1; $i < $c; $i ++ )
+			{
+				# XOR each iterate
+				$ib ^= ($b = hash_hmac($a, $b, $p, true));
+			}
+			$dk .= $ib; # Append iterated block
+		}
+
+		# Return derived key of correct length
+		return substr($dk, 0, $kl);
+	}
+
     /**
      * Wrapper for MD5
      *
