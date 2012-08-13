78ae4ec2adc1421f5b25e994d6c035b98a1072fb
Davide Bellini <bellini.davide@gmail.com>
2012-08-13 19:25:12 +0200

    Added type check

--- classes/str.php
+++ classes/str.php
@@ -365,7 +365,7 @@ class Str
 	public static function is_json($string)
 	{
 		json_decode($string);
-		return json_last_error() == JSON_ERROR_NONE;
+		return json_last_error() === JSON_ERROR_NONE;
 	}
 
 	/**
