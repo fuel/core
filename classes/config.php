fuel/core

commit 6b0e64ce93127a375e0ec7bf1a791f31b5e71d20

Optimize Arr get default check

classes/config.php
@@ -164,15 +164,17 @@ class Config
 	 */
 	public static function get($item, $default = null)
 	{
+		static $default_check_value = hex2bin('DEADBEEFCAFE');
+		
 		if (isset(static::$items[$item]))
 		{
 			return static::$items[$item];
 		}
 		elseif ( ! isset(static::$itemcache[$item]))
 		{
-			$val = \Fuel::value(\Arr::get(static::$items, $item, hex2bin('DEADBEEFCAFE')));
+			$val = \Fuel::value(\Arr::get(static::$items, $item, $default_check_value));
 			
-			if ($val === hex2bin('DEADBEEFCAFE'))
+			if ($val === $default_check_value)
 			{
 				return $default;
 			}

commit 26fe1bfb34b379842cf965f47dcdbb0fac6725ea

fixed bugs introduced by 6b0e64ce93127a375e0ec7bf1a791f31b5e71d20. Closes #1123

classes/config.php
@@ -27,6 +27,11 @@ class Config
 	public static $items = array();
 
 	/**
+	 * @var    string    $default_check_value          random value used as a not-found check in get()
+	 */
+	public static $default_check_value;
+
+	/**
 	 * @var    array    $itemcache       the dot-notated item cache
 	 */
 	protected static $itemcache = array();
@@ -164,21 +169,21 @@ class Config
 	 */
 	public static function get($item, $default = null)
 	{
-		static $default_check_value = hex2bin('DEADBEEFCAFE');
-		
+		is_null(static::$default_check_value) and static::$default_check_value = pack('H*', 'DEADBEEFCAFE');
+
 		if (isset(static::$items[$item]))
 		{
 			return static::$items[$item];
 		}
 		elseif ( ! isset(static::$itemcache[$item]))
 		{
-			$val = \Fuel::value(\Arr::get(static::$items, $item, $default_check_value));
-			
-			if ($val === $default_check_value)
+			$val = \Fuel::value(\Arr::get(static::$items, $item, static::$default_check_value));
+
+			if ($val === static::$default_check_value)
 			{
 				return $default;
 			}
-			
+
 			static::$itemcache[$item] = $val;
 		}
 