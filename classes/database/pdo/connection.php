666f37d67a09e9066e63cacbae077d71210158d4
Michiel Vugteveen <michiel@it-can.nl>
2012-08-31 13:01:50 +0200

    client compression

--- classes/database/pdo/connection.php
+++ classes/database/pdo/connection.php
@@ -58,6 +58,7 @@ class Database_PDO_Connection extends \Database_Connection
 			'username'   => null,
 			'password'   => null,
 			'persistent' => false,
+			'compress'	 => true,
 		));
 
 		// Clear the connection parameters for security
@@ -74,6 +75,12 @@ class Database_PDO_Connection extends \Database_Connection
 		{
 			// Make the connection persistent
 			$attrs[\PDO::ATTR_PERSISTENT] = true;
+		}
+
+		if ($compress)
+		{
+			// Use client compression (doesn't work with mysqlnd)
+			$attrs[\PDO::MYSQL_ATTR_COMPRESS] = true;
 		}
 
 		try
