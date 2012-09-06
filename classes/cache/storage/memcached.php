3b6757e784b5fbdb30be745bb631bce2f3e5f253
WanWizard <wanwizard@wanwizard.eu>
2012-09-06 22:39:06 +0200

    fixed memcached cache dependency check not working. Closes #1101

--- classes/cache/storage/memcached.php
+++ classes/cache/storage/memcached.php
@@ -148,7 +148,7 @@ class Cache_Storage_Memcached extends \Cache_Storage_Driver
 			$key = isset($index[$identifier][0]) ? $index[$identifier] : false;
 
 			// key found and newer?
-			if ($key !== false and $key[1] > $this->created)
+			if ($key === false or $key[1] > $this->created)
 			{
 				return false;
 			}
