3ddaf31b15f2f2c87e48ff1109b1ecd53a548781
WanWizard <wanwizard@wanwizard.eu>
2012-09-07 00:30:46 +0200

    fixed cache dependency checks for APC and Redis drivers. Related #1101

--- classes/cache/storage/apc.php
+++ classes/cache/storage/apc.php
@@ -125,7 +125,7 @@ class Cache_Storage_Apc extends \Cache_Storage_Driver
 			$key = isset($index[$identifier][0]) ? $index[$identifier] : false;
 
 			// key found and newer?
-			if ($key !== false and $key[1] > $this->created)
+			if ($key === false or $key[1] > $this->created)
 			{
 				return false;
 			}
