f872c61461e02985b2a5ba0ab4ce129551781c81
WanWizard <wanwizard@wanwizard.eu>
2012-09-09 13:20:51 +0200

    bumped version to 1.4-dev

--- classes/fuel.php
+++ classes/fuel.php
@@ -29,7 +29,7 @@ class Fuel
 	/**
 	 * @var  string  The version of Fuel
 	 */
-	const VERSION = '1.3-dev';
+	const VERSION = '1.4-dev';
 
 	/**
 	 * @var  string  constant used for when in testing mode
