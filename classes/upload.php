bfd06dca53f9ddeb1882cf6b96c5588daa2b26fe
kenjis <kenji.uui@gmail.com>
2012-09-20 14:01:14 +0900

    Fix auto_rename file checking logic
    There is a bug if there are uploadings with the same filename at the same time, uploaded files may be overwritten even if set auto_rename true.
    
    Fix it by creating file with fopen 'x' and ensure not to be overwritten.
    
    Signed-off-by: Kenji Suzuki <kenji.uui@gmail.com>

--- classes/upload.php
+++ classes/upload.php
@@ -596,7 +596,8 @@ class Upload
 
 
 			// check if the file already exists
-			if (file_exists($path.implode('', $save_as)))
+			$fp = @fopen($path.implode('', $save_as), 'x');
+			if ($fp === false)
 			{
 				if ( (bool) static::$config['auto_rename'])
 				{
@@ -604,8 +605,9 @@ class Upload
 					do
 					{
 						$save_as[3] = '_'.++$counter;
+						$fp = @fopen($path.implode('', $save_as), 'x');
 					}
-					while (file_exists($path.implode('', $save_as)));
+					while ($fp === false);
 				}
 				else
 				{
@@ -617,6 +619,7 @@ class Upload
 					}
 				}
 			}
+			fclose($fp);
 
 			// no need to store it as an array anymore
 			$save_as = implode('', $save_as);
