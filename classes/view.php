a7a20895041ce0c3e06032cf724cbf35ffe52c43
WanWizard <wanwizard@wanwizard.eu>
2012-09-23 15:19:51 +0200

    View::get() returns all set variables if no key is given. Closes #1145

--- classes/view.php
+++ classes/view.php
@@ -393,6 +393,8 @@ class View
 	 *
 	 *     $value = $view->get('foo', 'bar');
 	 *
+	 * If the key is not given or null, the entire data array is returned.
+	 *
 	 * If a default parameter is not given and the variable does not
 	 * exist, it will throw an OutOfBoundsException.
 	 *
@@ -401,9 +403,13 @@ class View
 	 * @return  mixed
 	 * @throws  OutOfBoundsException
 	 */
-	public function &get($key, $default = null)
+	public function &get($key = null, $default = null)
 	{
-		if (array_key_exists($key, $this->data))
+		if (func_num_args() === 0 or $key === null)
+		{
+			return $this->data;
+		}
+		elseif (array_key_exists($key, $this->data))
 		{
 			return $this->data[$key];
 		}
