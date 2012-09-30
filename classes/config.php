1799a2155abcdf04f1d8eec2f8017d0303100b85
WanWizard <wanwizard@wanwizard.eu>
2012-09-30 18:27:27 +0200

    fixed incorrect return value for Config::load() when $group == true

--- classes/config.php
+++ classes/config.php
@@ -51,11 +51,11 @@ class Config
 		     ! is_object($file) and
 		    array_key_exists($file, static::$loaded_files))
 		{
+			$group === true and $group = $file;
 			if ($group === null or $group === false or ! isset(static::$items[$group]))
 			{
 				return false;
 			}
-			$group === true and $group = $file;
 			return static::$items[$group];
 		}
 
