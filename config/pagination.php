602dfd3f131def39b1fae8c511c3f23fed10b91c
WanWizard <wanwizard@wanwizard.eu>
2012-10-24 21:01:16 +0200

    added missing default pagination config file

---
+++ config/pagination.php
@@ -0,0 +1,31 @@
+<?php
+
+return array(
+
+	'template' => array(
+		'wrapper_start'           => '<div class="pagination"> ',
+		'wrapper_end'             => ' </div>',
+		'page_start'              => '<span class="page-links"> ',
+		'page_end'                => ' </span>',
+		'previous_start'          => '<span class="previous"> ',
+		'previous_end'            => ' </span>',
+		'previous_inactive_start' => ' <span class="previous-inactive">',
+		'previous_inactive_end'   => ' </span>',
+		'previous_inactive_attrs' => array(),
+		'previous_mark'           => '&laquo; ',
+		'previous_attrs'          => array(),
+		'next_start'              => '<span class="next"> ',
+		'next_end'                => ' </span>',
+		'next_inactive_start'     => ' <span class="next-inactive">',
+		'next_inactive_end'       => ' </span>',
+		'next_inactive_attrs'     => array(),
+		'next_mark'               => ' &raquo;',
+		'next_attrs'              => array(),
+		'active_start'            => '<span class="active"> ',
+		'active_end'              => ' </span>',
+		'active_attrs'            => array(),
+		'regular_start'           => '',
+		'regular_end'             => '',
+		'regular_attrs'           => array(),
+	),
+);
