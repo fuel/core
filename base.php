e09e42b16e67d857b59b10267619cebc7ce762e9
Luke Armstrong <me@lukearmstrong.co.uk>
2012-10-24 12:40:17 +0100

    Fuel\Core\Error is already loaded, just need to alias PhpErrorException

--- base.php
+++ base.php
@@ -363,8 +363,6 @@ if ( ! function_exists('load_error_classes'))
 	{
 		class_exists('Fuel\\Core\\Error') or import('error');
 		class_exists('Error') or class_alias('Fuel\\Core\\Error', 'Error');
-
-		class_exists('Fuel\\Core\\PhpErrorException') or import('phperrorexception');
 		class_exists('PhpErrorException') or class_alias('Fuel\\Core\\PhpErrorException', 'PhpErrorException');
 
 		class_exists('Fuel\\Core\\Debug') or import('debug');
