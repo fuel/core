740ab97b91ff436a7e34320c3065e86e2a1f1252
WanWizard <wanwizard@wanwizard.eu>
2012-11-02 14:15:11 +0100

    moved all default configuration into core/config

--- config/pagination.php
+++ config/pagination.php
@@ -1,4 +1,24 @@
 <?php
+/**
+ * Part of the Fuel framework.
+ *
+ * @package    Fuel
+ * @version    1.0
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2012 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+/**
+ * NOTICE:
+ *
+ * If you need to make modifications to the default configuration, copy
+ * this file to your app/config folder, and make them in there.
+ *
+ * This will allow you to upgrade fuel without losing your custom config.
+ */
+
 
 return array(
 
