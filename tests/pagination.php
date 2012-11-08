7295c2012d35eed8376dfac81ca83d47472a6cbb
WanWizard <wanwizard@wanwizard.eu>
2012-11-09 00:14:08 +0100

    removed Pagination test, can't be tested without DiC

--- tests/pagination.php
+++ tests/pagination.php
@@ -13,69 +13,12 @@
 namespace Fuel\Core;
 
 /**
- * Pagination class tests
+ * Request class tests
  *
  * @group Core
- * @group Pagination
+ * @group Request
  */
 class Test_Pagination extends TestCase
 {
-	public function setup()
-	{
-		Pagination::set_config(array(
-			'pagination_url' => 'http://docs.fuelphp.com/',
-			'uri_segment' => 2,
-			'total_items' => 10,
-			'per_page' => 5,
-		));
-	}
-
-	/**
-	 * first page: previous inactive
-	 *
-	 */
-	public function test_previouslink_inactive()
-	{
-		Pagination::set_config(array(
-			'current_page' => 1,
-		));
-		$output = Pagination::prev_link('prev');
-		$expected = ' <span class="previous-inactive"><a href="#">&laquo; prev</a> </span>';
-		$this->assertEquals($expected, $output);
-	}
-
-	public function test_previouslink_active()
-	{
-		Pagination::set_config(array(
-			'current_page' => 2,
-		));
-		$output = Pagination::prev_link('prev');
-		$expected = '<span class="previous"> <a href="http://docs.fuelphp.com">&laquo; prev</a> </span>';
-		$this->assertEquals($expected, $output);
-	}
-
-	public function test_nextlink_active()
-	{
-		Pagination::set_config(array(
-			'current_page' => 1,
-		));
-		$output = Pagination::next_link('next');
-		$expected = '<span class="next"> <a href="http://docs.fuelphp.com/2">next &raquo;</a> </span>';
-		$this->assertEquals($expected, $output);
-	}
-
-	/**
-	 * last page: next inactive
-	 *
-	 */
-	public function test_nextlink_inactive()
-	{
-		Pagination::set_config(array(
-			'current_page' => 2,
-		));
-		$output = Pagination::next_link('next');
-		$expected = ' <span class="next-inactive"><a href="#">next &raquo;</a> </span>';
-		$this->assertEquals($expected, $output);
-	}
-
+ 	public function test_foo() {}
 }
