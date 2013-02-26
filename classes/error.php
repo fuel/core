4acccbf2336c55b66f715a870bf28cf12473e70c
WanWizard <wanwizard@wanwizard.eu>
2013-02-26 13:01:24 +0100

    fixed bug in the error handler that made trigger_error() calls not catchable

--- classes/error.php
+++ classes/error.php
@@ -19,7 +19,10 @@ class PhpErrorException extends \ErrorException
 {
 	public static $count = 0;
 
-	public function handle()
+	/**
+	 * Allow the error handler from recovering from error types defined in the config
+	 */
+	public function recover()
 	{
 		// handle the error based on the config and the environment we're in
 		if (static::$count <= Config::get('errors.throttle', 10))
@@ -145,8 +148,9 @@ class Error
 			}
 			else
 			{
+				// non-fatal, recover from the error
 				$e = new \PhpErrorException($message, $severity, 0, $filepath, $line);
-				$e->handle();
+				$e->recover();
 			}
 		}
 
