32fe6f55ed3ed98a4ce1299e9de754dcc4106344
WanWizard <wanwizard@wanwizard.eu>
2013-04-11 16:28:55 +0200

    fixed pagination on installs with a modified form config

--- tests/pagination.php
+++ tests/pagination.php
@@ -69,10 +69,45 @@ class Test_Pagination extends TestCase
 	protected function set_uri_segment_config()
 	{
 		$this->config = array(
-			'uri_segment'    => 3,
-			'pagination_url' => 'http://docs.fuelphp.com/welcome/index/',
-			'total_items'    => 100,
-			'per_page'       => 10,
+			'uri_segment'             => 3,
+			'pagination_url'          => 'http://docs.fuelphp.com/welcome/index/',
+			'total_items'             => 100,
+			'per_page'                => 10,
+			'wrapper'                 => "<div class=\"pagination\">\n\t{pagination}\n</div>\n",
+
+			'first'                   => "<span class=\"first\">\n\t{link}\n</span>\n",
+			'first-marker'            => "&laquo;&laquo;",
+			'first-link'              => "\t\t<a href=\"{uri}\">{page}</a>\n",
+
+			'first-inactive'          => "",
+			'first-inactive-link'     => "",
+
+			'previous'                => "<span class=\"previous\">\n\t{link}\n</span>\n",
+			'previous-marker'         => "&laquo;",
+			'previous-link'           => "\t\t<a href=\"{uri}\" rel=\"prev\">{page}</a>\n",
+
+			'previous-inactive'       => "<span class=\"previous-inactive\">\n\t{link}\n</span>\n",
+			'previous-inactive-link'  => "\t\t<a href=\"#\" rel=\"prev\">{page}</a>\n",
+
+			'regular'                 => "<span>\n\t{link}\n</span>\n",
+			'regular-link'            => "\t\t<a href=\"{uri}\">{page}</a>\n",
+
+			'active'                  => "<span class=\"active\">\n\t{link}\n</span>\n",
+			'active-link'             => "\t\t<a href=\"#\">{page}</a>\n",
+
+			'next'                    => "<span class=\"next\">\n\t{link}\n</span>\n",
+			'next-marker'            => "&raquo;",
+			'next-link'               => "\t\t<a href=\"{uri}\" rel=\"next\">{page}</a>\n",
+
+			'next-inactive'           => "<span class=\"next-inactive\">\n\t{link}\n</span>\n",
+			'next-inactive-link'      => "\t\t<a href=\"#\" rel=\"next\">{page}</a>\n",
+
+			'last'                    => "<span class=\"last\">\n\t{link}\n</span>\n",
+			'last-marker'             => "&raquo;&raquo;",
+			'last-link'               => "\t\t<a href=\"{uri}\">{page}</a>\n",
+
+			'last-inactive'           => "",
+			'last-inactive-link'      => "",
 		);
 	}
 
@@ -375,10 +410,45 @@ class Test_Pagination extends TestCase
 	protected function set_query_string_config()
 	{
 		$this->config = array(
-			'uri_segment'    => 'p',
-			'pagination_url' => 'http://docs.fuelphp.com/',
-			'total_items'    => 100,
-			'per_page'       => 10,
+			'uri_segment'             => 'p',
+			'pagination_url'          => 'http://docs.fuelphp.com/',
+			'total_items'             => 100,
+			'per_page'                => 10,
+			'wrapper'                 => "<div class=\"pagination\">\n\t{pagination}\n</div>\n",
+
+			'first'                   => "<span class=\"first\">\n\t{link}\n</span>\n",
+			'first-marker'            => "&laquo;&laquo;",
+			'first-link'              => "\t\t<a href=\"{uri}\">{page}</a>\n",
+
+			'first-inactive'          => "",
+			'first-inactive-link'     => "",
+
+			'previous'                => "<span class=\"previous\">\n\t{link}\n</span>\n",
+			'previous-marker'         => "&laquo;",
+			'previous-link'           => "\t\t<a href=\"{uri}\" rel=\"prev\">{page}</a>\n",
+
+			'previous-inactive'       => "<span class=\"previous-inactive\">\n\t{link}\n</span>\n",
+			'previous-inactive-link'  => "\t\t<a href=\"#\" rel=\"prev\">{page}</a>\n",
+
+			'regular'                 => "<span>\n\t{link}\n</span>\n",
+			'regular-link'            => "\t\t<a href=\"{uri}\">{page}</a>\n",
+
+			'active'                  => "<span class=\"active\">\n\t{link}\n</span>\n",
+			'active-link'             => "\t\t<a href=\"#\">{page}</a>\n",
+
+			'next'                    => "<span class=\"next\">\n\t{link}\n</span>\n",
+			'next-marker'            => "&raquo;",
+			'next-link'               => "\t\t<a href=\"{uri}\" rel=\"next\">{page}</a>\n",
+
+			'next-inactive'           => "<span class=\"next-inactive\">\n\t{link}\n</span>\n",
+			'next-inactive-link'      => "\t\t<a href=\"#\" rel=\"next\">{page}</a>\n",
+
+			'last'                    => "<span class=\"last\">\n\t{link}\n</span>\n",
+			'last-marker'             => "&raquo;&raquo;",
+			'last-link'               => "\t\t<a href=\"{uri}\">{page}</a>\n",
+
+			'last-inactive'           => "",
+			'last-inactive-link'      => "",
 		);
 	}
 
