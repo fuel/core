0e063be6115132670fd20f62d20630e6c9bd3933
WanWizard <wanwizard@wanwizard.eu>
2013-05-03 21:59:23 +0200

    bumped the version to 1.7-dev

--- classes/fuel.php
+++ classes/fuel.php
@@ -29,7 +29,7 @@ class Fuel
 	/**
 	 * @var  string  The version of Fuel
 	 */
-	const VERSION = '1.6';
+	const VERSION = '1.7-dev';
 
 	/**
 	 * @var  string  constant used for when in testing mode
