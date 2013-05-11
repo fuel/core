da3d8f74a0bc96d7ab921d56141c79e069627c41
WanWizard <wanwizard@wanwizard.eu>
2013-05-11 12:00:48 +0200

    make sure Agent tests can run with known test values

--- tests/agent.php
+++ tests/agent.php
@@ -26,9 +26,13 @@ class Test_Agent extends TestCase
 	 */
 	protected function setUp()
 	{
+		// make sure we've got the Agent class in a known state
 		$_SERVER['HTTP_ACCEPT_LANGUAGE'] = 'en-us,en;q=0.8,nl-be;q=0.5,nl;q=0.3';
 		$_SERVER['HTTP_ACCEPT_CHARSET'] = 'UTF-8,ISO-8859-1,*';
 		$_SERVER['HTTP_USER_AGENT'] = 'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.16) Gecko/20110322 Fedora/3.6.16-1.fc14 Firefox/3.6.16';
+
+		// by re-running the class init manually
+		\Agent::_init();
 	}
 
 	/**
