3ee64fe20108c9d86f3ad67970c03efc06eb752e
Fabien Foulgoc <fbn.fgc+github@gmail.com>
2013-08-05 10:41:54 +0200

    Backup date default timezone before using date_default_timezone_set in Date unit test.

--- tests/date.php
+++ tests/date.php
@@ -76,10 +76,13 @@ class Test_Date extends TestCase
 	 */
 	public function test_format()
 	{
+		$default_timezone = date_default_timezone_get();
 		date_default_timezone_set('UTC');
 
 		$output = Date::forge( 1294176140 )->format("%m/%d/%Y");
 		$expected = "01/04/2011";
+
+		date_default_timezone_set($default_timezone);
 
 		$this->assertEquals($expected, $output);
 	}
