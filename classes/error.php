6f9bc6957765c2599be660fa0a854bcbf3103113
WanWizard <wanwizard@wanwizard.eu>
2013-08-09 09:43:31 +0200

    make the possible error list complete

--- classes/error.php
+++ classes/error.php
@@ -52,19 +52,22 @@ class Error
 {
 
 	public static $levels = array(
-		0                  => 'Error',
-		E_ERROR            => 'Error',
-		E_WARNING          => 'Warning',
-		E_PARSE            => 'Parsing Error',
-		E_NOTICE           => 'Notice',
-		E_CORE_ERROR       => 'Core Error',
-		E_CORE_WARNING     => 'Core Warning',
-		E_COMPILE_ERROR    => 'Compile Error',
-		E_COMPILE_WARNING  => 'Compile Warning',
-		E_USER_ERROR       => 'User Error',
-		E_USER_WARNING     => 'User Warning',
-		E_USER_NOTICE      => 'User Notice',
-		E_STRICT           => 'Runtime Notice'
+		0                   => 'Error',
+		E_ERROR             => 'Error',
+		E_WARNING           => 'Warning',
+		E_PARSE             => 'Parsing Error',
+		E_NOTICE            => 'Notice',
+		E_CORE_ERROR        => 'Core Error',
+		E_CORE_WARNING      => 'Core Warning',
+		E_COMPILE_ERROR     => 'Compile Error',
+		E_COMPILE_WARNING   => 'Compile Warning',
+		E_USER_ERROR        => 'User Error',
+		E_USER_WARNING      => 'User Warning',
+		E_USER_NOTICE       => 'User Notice',
+		E_STRICT            => 'Runtime Notice',
+		E_RECOVERABLE_ERROR => 'Runtime Recoverable error',
+		E_DEPRECATED        => 'Runtime Deprecated code usage',
+		E_USER_DEPRECATED   => 'User Deprecated code usage',
 	);
 
 	public static $fatal_levels = array(E_PARSE, E_ERROR, E_USER_ERROR, E_COMPILE_ERROR);
