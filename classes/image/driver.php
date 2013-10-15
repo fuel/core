81d9e06cc4f4691a7c9f3528c8b5a9cef4bafa26
WanWizard <wanwizard@wanwizard.eu>
2013-10-15 11:11:49 +0200

    reverted call_user_func_array change; this could call internal protected methods

--- classes/image/driver.php
+++ classes/image/driver.php
@@ -855,7 +855,7 @@ abstract class Image_Driver
 				$tmpfunc[$i] = var_export($action[$i], true);
 			}
 			$this->debug('', "<b>Executing <code>" . implode(", ", $tmpfunc) . "</code></b>");
-			call_fuel_func_array(array(&$this, '_' . $action[0]), array_slice($action, 1));
+			call_user_func_array(array(&$this, '_' . $action[0]), array_slice($action, 1));
 		}
 		if (($clear === null and $this->config['clear_queue']) or $clear === true)
 		{
