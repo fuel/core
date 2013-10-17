ef97f861f31ede4347f1d3574c84cfecd86f4737
Alastair McGowan-Douglas <alastair@propcom.co.uk>
2013-10-17 11:35:36 +0100

    Only output CLI backtrace when config says to

--- classes/error.php
+++ classes/error.php
@@ -189,8 +189,10 @@ class Error
 		if (\Fuel::$is_cli)
 		{
 			\Cli::write(\Cli::color($data['severity'].' - '.$data['message'].' in '.\Fuel::clean_path($data['filepath']).' on line '.$data['error_line'], 'red'));
-			\Cli::write('Stack trace:');
-			\Cli::write(\Debug::backtrace($e->getTrace()));
+			if (\Config::get('cli_backtrace')) {
+				\Cli::write('Stack trace:');
+				\Cli::write(\Debug::backtrace($e->getTrace()));
+			}
 			return;
 		}
 
