ecac928bf73511365e2f14876020046684b18af3
KOYAMA Tetsuji <koyama@hoge.org>
2013-10-26 17:05:51 +0900

    Import japanese language messages file for validation.

---
+++ lang/ja/validation.php
@@ -0,0 +1,21 @@
+<?php
+
+return array(
+	'required'        => ':label を入力して下さい。',
+	'min_length'      => ':label は、少なくとも :param:1 文字以上入力して下さい。',
+	'max_length'      => ':label は、:param:1 文字以下で入力して下さい。',
+	'exact_length'    => ':label は、:param:1 文字で入力して下さい。',
+	'match_value'     => ':label は、使用できない文字が含まれています。使用可能な文字 [:param:1] ',
+	'match_pattern'   => ':label は、パターン(:param:1)にマッチした文字を入力して下さい。',
+	'match_field'     => ':label は、:param:1 と異なっています。',
+	'valid_email'     => ':label は、有効なメールアドレスではありません。',
+	'valid_emails'    => ':label に、有効でないメールアドレスが含まれています。',
+	'valid_url'       => ':label は、有効なURLではありません。',
+	'valid_ip'        => ':label は、有効なIPアドレスではありません。',
+	'numeric_min'     => ':label には、:param:1 より大きい数値を入力して下さい。',
+	'numeric_max'     => ':label には、:param:1 より小さい数値を入力して下さい。',
+	'numeric_between' => ':label には、:param:1 と :param:2 の間の数値を入力して下さい。',
+	'valid_string'    => ':label は、有効な文字列ではありません。:rule で入力して下さい。',
+	'required_with'   => ':param:1 が含まれる場合は、:label も必ず含まれなければなりません。',
+	'valid_date'      => ':label は、正しい日付ではありません。',
+);
