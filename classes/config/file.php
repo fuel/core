2b8aa801936d4f47f3a1ea7a8480872119815c48
WanWizard <wanwizard@wanwizard.eu>
2013-12-20 18:12:59 +0100

    added missing config file extension

--- classes/config/file.php
+++ classes/config/file.php
@@ -164,7 +164,7 @@ abstract class Config_File implements Config_Interface
 					$this->file = substr($this->file, $pos+2);
 
 					// strip the classes directory as we need the module root
-					$path = substr($path,0, -8).'config'.DS.$this->file;
+					$path = substr($path,0, -8).'config'.DS.$this->file.$this->ext;
 				}
 				else
 				{
