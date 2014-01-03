fuel/core

commit cd9260fc42345854d948a8752137eafadd32168c

deal with array responses on non-restful requests

classes/controller/hybrid.php
@@ -108,7 +108,7 @@ abstract class Controller_Hybrid extends \Controller_Rest
 
 			if ( ! $response instanceof Response)
 			{
-				$response = \Response::forge($response, $this->response_status);
+				$response = $this->response($response);
 			}
 		}
 

commit d6b4dfe32f278cbd00dd93503167ab10863f77b9

fixed BC, broken in cd9260fc42345854d948a8752137eafadd32168c

classes/controller/hybrid.php
@@ -106,9 +106,16 @@ abstract class Controller_Hybrid extends \Controller_Rest
 				}
 			}
 
+			// deal with returned array's in non-restful calls
+			elseif (is_array($response))
+			{
+				$response = \Format::forge()->to_json($response, true);
+			}
+
+			// and make sure we have a valid Response object
 			if ( ! $response instanceof Response)
 			{
-				$response = $this->response($response);
+				$response = \Response::forge($response, $this->response_status);
 			}
 		}
 