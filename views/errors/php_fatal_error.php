872e7c7e0910a7ab5347b3c171e1d1d6ad082ccc
Amal Francis <amalfra@gmail.com>
2014-02-20 21:14:34 +0530

    Update php_fatal_error.php
    A tiny border to wrapper. I think it would look the php fatal error view look bit better....

--- views/errors/php_fatal_error.php
+++ views/errors/php_fatal_error.php
@@ -6,7 +6,7 @@
 	<style type="text/css">
 		* { margin: 0; padding: 0; }
 		body { background-color: #EEE; font-family: sans-serif; font-size: 16px; line-height: 20px; margin: 40px; }
-		#wrapper { padding: 30px; background: #fff; color: #333; margin: 0 auto; width: 800px; }
+		#wrapper { padding: 30px; background: #fff; color: #333; border: 1px solid #ccc; margin: 0 auto; width: 800px; }
 		a { color: #36428D; }
 		h1 { color: #000; font-size: 55px; padding: 0 0 25px; line-height: 1em; }
 		.intro { font-size: 22px; line-height: 30px; font-family: georgia, serif; color: #555; padding: 29px 0 20px; border-top: 1px solid #CCC; }
