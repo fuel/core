17e6e50b211cc0ad9c90d11fdee7688810057252
sebesvn <systemseb@gmail.com>
2014-03-19 11:48:49 +0100

    fixing class name comparison in SSH2.php

--- vendor/phpseclib/Net/SSH2.php
+++ vendor/phpseclib/Net/SSH2.php
@@ -1311,7 +1311,7 @@ class Net_SSH2 {
         }
 
         // although PHP5's get_class() preserves the case, PHP4's does not
-        if (is_object($password) && strtolower(get_class($password)) == 'crypt_rsa') {
+        if (is_object($password) && strtolower(get_class($password)) == 'phpseclib\crypt_rsa') {
             return $this->_privatekey_login($username, $password);
         }
 
