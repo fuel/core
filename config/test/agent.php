b04c3917e75b33867be96d1ddc29d31a49540c05
Burhan Zainuddin <burhan@codeyellow.nl>
2014-06-20 12:51:12 +0200

    Fixed unit test for agent so that it uses the correct browscap.ini file.

---
+++ config/test/agent.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+    'browscap' => array(
+        'enabled' => true,
+        'url' => '',
+        'method' => 'local',
+        'file' => __DIR__.DS.'..'.DS.'..'.DS.'tests'.DS.'agent'.DS.'browscap.ini',
+    ),
+    'cache' => array(
+        'driver' => '',
+        'expiry' => 1,
+        'identifier' => 'fuel.agent-test',
+    )
+);
