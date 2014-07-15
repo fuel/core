42cb98f3e537b01514b0ec7ebdac0a24b7c86b9a
Michiel Vugteveen <info@it-can.nl>
2014-07-15 15:11:12 +0200

    namespace fixes

--- config/config.php
+++ config/config.php
@@ -120,12 +120,12 @@ return array(
 	/**
 	 * Logging Threshold.  Can be set to any of the following:
 	 *
-	 * Fuel::L_NONE
-	 * Fuel::L_ERROR
-	 * Fuel::L_WARNING
-	 * Fuel::L_DEBUG
-	 * Fuel::L_INFO
-	 * Fuel::L_ALL
+	 * \Fuel::L_NONE
+	 * \Fuel::L_ERROR
+	 * \Fuel::L_WARNING
+	 * \Fuel::L_DEBUG
+	 * \Fuel::L_INFO
+	 * \Fuel::L_ALL
 	 */
 	'log_threshold'    => \Fuel::L_WARNING,
 
