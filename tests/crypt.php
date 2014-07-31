4d0ee161aaad2849b87a2451bab46aea6b5c563b
kenjis <kenji.uui@gmail.com>
2014-07-31 10:26:13 +0900

    Add tests for Crypt
    Signed-off-by: Kenji Suzuki <kenji.uui@gmail.com>

--- tests/crypt.php
+++ tests/crypt.php
@@ -20,5 +20,36 @@ namespace Fuel\Core;
  */
 class Test_Crypt extends TestCase
 {
- 	public function test_foo() {}
+	public function setup_config()
+	{
+		\Config::load('crypt', true);
+		$this->config_backup = \Config::get('crypt');
+		\Config::set('crypt.crypto_iv', 'tzcPXg2LEnB8vysdKw_Tsjo4');
+		\Config::set('crypt.crypto_hmac', 'jX4p30_hYm7U-a85vov_M0P4');
+		\Crypt::_init();
+	}
+
+	public function restore_config()
+	{
+		foreach ($this->config_backup as $key => $val)
+		{
+			\Config::set($key, $val);
+		}
+		\Crypt::_init();
+	}
+
+	public function test_encode_decode()
+	{
+		$this->setup_config();
+
+		$encoded = 'QE4AhW52POclh6PgPIVW5Xk3azVreHFfQjVmLXdVamNjbkZyZlE4YVQwQ1Fmc0Z3NE1fZlM0WDR4X00';
+		$clear_text = 'some string';
+		$test = \Crypt::encode($clear_text, 'this_is_key');
+		$this->assertEquals($encoded, $test);
+
+		$test = \Crypt::decode($encoded, 'this_is_key');
+		$this->assertEquals($clear_text, $test);
+
+		$this->restore_config();
+	}
 }
