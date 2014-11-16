d6ebeb77496326cf9b06d064f9030971142ec750
WanWizard <wanwizard@wanwizard.eu>
2014-11-16 17:27:29 +0100

    when a view is not found, also show the extension

--- classes/view.php
+++ classes/view.php
@@ -390,7 +390,7 @@ class View
 		// locate the view file
 		if (($path = \Finder::search('views', $file, '.'.$this->extension, false, false)) === false)
 		{
-			throw new \FuelException('The requested view could not be found: '.\Fuel::clean_path($file));
+			throw new \FuelException('The requested view could not be found: '.\Fuel::clean_path($file).'.'.$this->extension);
 		}
 
 		// Store the file path locally
