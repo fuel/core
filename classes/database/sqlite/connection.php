e8f23453e315ee6a0572837bd853e2e5d3d20ee2
WanWizard <wanwizard@wanwizard.eu>
2016-07-20 09:45:39 +0100

    woops, forgot brackets...

--- classes/database/sqlite/connection.php
+++ classes/database/sqlite/connection.php
@@ -26,7 +26,7 @@ class Database_SQLite_Connection extends \Database_PDO_Connection
 	 */
 	public function update($table = null)
 	{
-		return new Database_SQLite_Builder_Update($table)->set_connection($this);
+		return (new Database_SQLite_Builder_Update($table))->set_connection($this);
 	}
 
 	/**
@@ -40,7 +40,7 @@ class Database_SQLite_Connection extends \Database_PDO_Connection
 	 */
 	public function delete($table = null)
 	{
-		return new Database_SQLite_Builder_Delete($table)->set_connection($this);
+		return (new Database_SQLite_Builder_Delete($table))->set_connection($this);
 	}
 
 	/**
