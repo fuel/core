39fd019c3325a32db87a3e921cb7dac181f3c37a
WanWizard <wanwizard@wanwizard.eu>
2016-09-06 16:57:22 +0200

    add support for multipart/form-data, and fixed raw input of json and xml input

--- classes/input/instance.php
+++ classes/input/instance.php
@@ -408,25 +408,81 @@ class Input_Instance
 		$this->input_get = $_GET;
 		$this->input_post = $_POST;
 
+		// get the content type from the header
+		$content_type = \Input::headers('Content-Type');
+
 		// get php raw input
 		$php_input = file_get_contents('php://input');
-		if (strpos(\Input::headers('Content-Type'), 'www-form-urlencoded') > 0 and \Config::get('security.form-double-urlencoded', false))
+
+		// handle form-urlencoded input
+		if ($content_type == 'application/x-www-form-urlencoded')
 		{
-			$php_input = urldecode($php_input);
+			// urldecode it if needed
+			if (\Config::get('security.form-double-urlencoded', false))
+			{
+				$php_input = urldecode($php_input);
+			}
+			parse_str($php_input, $php_input);
 		}
 
-		// convert it to xml and json if needed
-		if (is_array($php_input))
+		// handle multipart/form-data input
+		elseif (strpos($content_type, 'multipart/form-data') === 0)
 		{
-			$this->input_xml = \Security::clean(\Format::forge($php_input, 'xml')->to_array());
-			$this->input_json = \Security::clean(\Format::forge($php_input, 'json')->to_array());
+			// grab multipart boundary from content type header
+			preg_match('/boundary=(.*)$/', $content_type, $matches);
+			$boundary = $matches[1];
+
+			// split content by boundary and get rid of last -- element
+			$blocks = preg_split('/-+'.$boundary.'/', $php_input);
+			array_pop($blocks);
+
+			// loop data blocks
+			$php_input = array();
+			foreach ($blocks as $id => $block)
+			{
+				// skip empty blocks
+				if ( ! empty($block))
+				{
+					// parse uploaded files
+					if (strpos($block, 'application/octet-stream') !== FALSE)
+					{
+						// match "name", then everything after "stream" (optional) except for prepending newlines
+						preg_match("/name=\"([^\"]*)\".*stream[\n|\r]+([^\n\r].*)?$/s", $block, $matches);
+					}
+					// parse all other fields
+					else
+					{
+						// match "name" and optional value in between newline sequences
+						preg_match('/name=\"([^\"]*)\"[\n|\r]+([^\n\r].*)?\r$/s', $block, $matches);
+					}
+
+					// store the result, if any
+					$php_input[$matches[1]] = isset($matches[2]) ? $matches[2] : '';
+				}
+			}
 		}
 
-		// store other input data as well
+		// handle json input
+		elseif ($content_type == 'application/json')
+		{
+			$this->input_json = $php_input = \Security::clean(\Format::forge($php_input, 'json')->to_array());
+		}
+
+		// handle xml input
+		elseif ($content_type == 'application/xml' or $content_type == 'text/xml')
+		{
+			$this->input_xml = $php_input = \Security::clean(\Format::forge($php_input, 'xml')->to_array());
+		}
+
+		// handle raw formats
+		else
+		{
+		}
+
+		// store it as other input data as well
 		if ($this->method() == 'PUT' or $this->method() == 'PATCH' or $this->method() == 'DELETE')
 		{
-			parse_str($php_input, $php_input);
-			$this->{'input_'.$this->method()} = $php_input;
+			$this->{'input_'.strtolower($this->method())} = $php_input;
 		}
 	}
 }
