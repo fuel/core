939b830ad7fad889cf8c176ee2006d319e16e7a4
WanWizard <wanwizard@wanwizard.eu>
2017-09-05 18:21:48 +0100

    updated testcase classes for fuel/core/database

--- tests/database/pdo/cached.php
+++ tests/database/pdo/cached.php
@@ -13,12 +13,12 @@
 namespace Fuel\Core;
 
 /**
- * Database_Result class tests
+ * Database_PDO_Cached class tests
  *
  * @group Core
  * @group Database
  */
-class Test_Database_Result extends TestCase
+class Test_Database_PDO_Cached extends TestCase
 {
  	public function test_foo() {}
 }
---
+++ tests/database/pdo/result.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Part of the Fuel framework.
+ *
+ * @package    Fuel
+ * @version    1.8
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2017 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+namespace Fuel\Core;
+
+/**
+ * Database_PDO_Result class tests
+ *
+ * @group Core
+ * @group Database
+ */
+class Test_Database_PDO_Result extends TestCase
+{
+ 	public function test_foo() {}
+}
