dcf45ef9188bc61cea9b8902198efe0af030762a
Illirgway <Illirgway@users.noreply.github.com>
2019-10-08 17:07:34 +0300

    Update result.php

--- classes/database/mysqli/result.php
+++ classes/database/mysqli/result.php
@@ -81,10 +81,7 @@ class Database_MySQLi_Result extends \Database_Result
 		}
 
 		// sanitize the data if needed
-		if ($this->_sanitization_enabled)
-		{
-			$this->_row = \Security::clean($this->_row, null, 'security.output_filter');
-		}
+		$this->_sanitizate();
 
 		return $this->_row;
 	}
