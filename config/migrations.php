e965c5319e85430ca8a6a18fa89b42f03b3e06b2
WanWizard <wanwizard@wanwizard.eu>
2025-05-02 14:45:40 +0100

    [migrate] add the option to create a file flag when migrations are in progress
    this can be used to prevent access to the application and/or display a
    maintenance page when migrations are running.

--- config/migrations.php
+++ config/migrations.php
@@ -83,4 +83,24 @@ return array(
 	 */
 
 	 'flush_cache' => false,
+
+	/**
+	 * -------------------------------------------------------------------------
+	 *  Flag
+	 * -------------------------------------------------------------------------
+	 *
+	 *  Name of a file to be used as "in-migration" flag. If configured, it will
+	 *  be created as soon as migrations start, and removed when migrations
+	 *  are finished. This can be used by the app to block access or show a
+	 *  maintenance page.
+	 *
+	 *  Note, the file must be in a location where both the CLI and the webserver
+	 *  is able to write to. /tmp is obvious, but will usually not work due to
+	 *  systemd PrivateTmp.
+	 *
+	 *  Default value is null.
+	 *
+	 */
+
+	 'flag' => null,
 );
