b42354c2bdd8ba5dc10c0a4038826159c64c2adb
Marc Bush <marcbush@users.noreply.github.com>
2025-09-26 13:56:27 +0100

    Fix case conversion function calls in str.php
    replace str_to_lower with strtolower

--- classes/str.php
+++ classes/str.php
@@ -133,7 +133,7 @@ class Str
 	{
 		if (PHP_VERSION_ID >= 80000)
 		{
-			return $ignore_case ? str_starts_with(str_to_lower($str), str_to_lower($start)) : str_starts_with($str, $start);
+			return $ignore_case ? str_starts_with(strtolower($str), strtolower($start)) : str_starts_with($str, $start);
 		}
 
 		return (bool) preg_match('/^'.preg_quote($start, '/').'/m'.($ignore_case ? 'i' : ''), (string) $str);
@@ -151,7 +151,7 @@ class Str
 	{
 		if (PHP_VERSION_ID >= 80000)
 		{
-			return $ignore_case ? str_ends_with(str_to_lower($str), str_to_lower($end)) : str_ends_with($str, $end);
+			return $ignore_case ? str_ends_with(strtolower($str), strtolower($end)) : str_ends_with($str, $end);
 		}
 
 		return (bool) preg_match('/'.preg_quote($end, '/').'$/m'.($ignore_case ? 'i' : ''), $str);
