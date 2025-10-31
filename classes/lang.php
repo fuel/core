a2dfe0794fd64b9806d4746bf1921bc85fbdc6ff
WanWizard <wanwizard@wanwizard.eu>
2025-10-31 16:25:00 +0000

    [lang] do not attempt to construct a filename if a FQFN is given

--- classes/lang.php
+++ classes/lang.php
@@ -165,8 +165,8 @@ class Lang
 	{
 		($language === null) and $language = static::get_lang();
 
-		// prefix the file with the language
-		if ( ! is_null($language))
+		// if file is not an FQFN, prefix the file with the language
+		if ( ! is_file($file) and ! is_null($language))
 		{
 			$file = explode('::', $file);
 			end($file);
